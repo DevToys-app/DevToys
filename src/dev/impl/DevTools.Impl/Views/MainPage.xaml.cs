dd91c3c895a749957adda1ec758e50d511f6e2b6
Etienne Baudoux <ebaudoux@outlook.com>
2021-09-05 23:05:51 -0700

    Added NavigationView

--- src/dev/impl/DevTools.Impl/Views/MainPage.xaml.cs
+++ src/dev/impl/DevTools.Impl/Views/MainPage.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using Windows.UI.Xaml.Controls;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace DevTools.Uwp
+namespace DevTools.Impl.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
