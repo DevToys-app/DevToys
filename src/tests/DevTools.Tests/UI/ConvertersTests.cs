4b2306f28978b56585f8a8dfcfcc78573fefd118
Etienne Baudoux <ebaudoux@outlook.com>
2021-09-11 22:28:40 -0700

    Added expandable setting control + settings UI

--- src/tests/DevTools.Tests/UI/ConvertersTests.cs
+++ src/tests/DevTools.Tests/UI/ConvertersTests.cs
@@ -1,4 +1,6 @@
-﻿using DevTools.Common.UI.Converters;
+﻿#nullable disable
+
+using DevTools.Common.UI.Converters;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Windows.UI.Xaml;
 
@@ -20,6 +22,31 @@ namespace DevTools.Tests.UI
 
             Assert.AreEqual(Visibility.Collapsed, converter.Convert(true, typeof(Visibility), null, null));
             Assert.AreEqual(Visibility.Visible, converter.Convert(false, typeof(Visibility), null, null));
+        }
+
+        [TestMethod]
+        public void NullToBooleanConverterTest()
+        {
+            var converter = new NullToBooleanConverter();
+            converter.IsInverted = false;
+
+            Assert.IsTrue((bool)converter.Convert(null, typeof(bool), null, null));
+            Assert.IsFalse((bool)converter.Convert(1, typeof(bool), null, null));
+
+            converter.IsInverted = true;
+
+            Assert.IsFalse((bool)converter.Convert(null, typeof(bool), null, null));
+            Assert.IsTrue((bool)converter.Convert(1, typeof(bool), null, null));
+
+            converter.IsInverted = false;
+
+            Assert.IsTrue((bool)converter.Convert(string.Empty, typeof(bool), null, null));
+            Assert.IsFalse((bool)converter.Convert("foo", typeof(bool), null, null));
+
+            converter.IsInverted = true;
+
+            Assert.IsFalse((bool)converter.Convert(string.Empty, typeof(bool), null, null));
+            Assert.IsTrue((bool)converter.Convert("foo", typeof(bool), null, null));
         }
     }
 }
