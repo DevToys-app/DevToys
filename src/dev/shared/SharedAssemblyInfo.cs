d604507445e964282a2163c008d9f5b91479e098
Etienne BAUDOUX <ebaudoux@outlook.com>
2021-09-26 15:46:14 -0700

    Fixed build issue

--- src/dev/shared/SharedAssemblyInfo.cs
+++ src/dev/shared/SharedAssemblyInfo.cs
@@ -34,7 +34,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2021.09.26.0")]
-[assembly: AssemblyFileVersion("2021.09.26.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
 
-[assembly: InternalsVisibleTo("DevTools.Tests")]
+[assembly: InternalsVisibleTo("DevToys.Tests")]
