f77e44f8dcb682ad9d85559547b35b710a6cc0b3
Etienne Baudoux <ebaudoux@velersoftware.com>
2021-10-05 01:00:45 -0700

    Added Json to Yaml tool (#16)
    * Added Json to Yaml tool
    
    * updated strings
    
    * Addressed feedback
    
    Co-authored-by: Etienne Baudoux <etbaudou@microsoft.com>

---
+++ src/tests/DevToys.Tests/Helpers/YamlHelperTests.cs
@@ -0,0 +1,20 @@
+﻿using DevToys.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevToys.Tests.Helpers
+{
+    [TestClass]
+    public class YamlHelperTests
+    {
+        [DataTestMethod]
+        [DataRow(null, false)]
+        [DataRow("", false)]
+        [DataRow(" ", false)]
+        [DataRow("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", false)]
+        [DataRow("foo :\n  bar :\n    - boo: 1\n    - rab: 2\n    - plop: 3", true)]
+        public void IsValidYaml(string input, bool expectedResult)
+        {
+            Assert.AreEqual(expectedResult, YamlHelper.IsValidYaml(input));
+        }
+    }
+}
