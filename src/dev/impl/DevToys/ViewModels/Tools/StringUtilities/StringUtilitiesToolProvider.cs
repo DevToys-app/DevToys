46d712aeda5512049e01af389974ace81f7550d9
Etienne BAUDOUX <ebaudoux@outlook.com>
2021-10-11 22:47:29 -0700

    Added String Utilities tool

---
+++ src/dev/impl/DevToys/ViewModels/Tools/StringUtilities/StringUtilitiesToolProvider.cs
@@ -0,0 +1,55 @@
+﻿#nullable enable
+
+using DevToys.Api.Core.Injection;
+using DevToys.Api.Tools;
+using DevToys.Core.Threading;
+using System.Composition;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace DevToys.ViewModels.Tools.StringUtilities
+{
+    [Export(typeof(IToolProvider))]
+    [Name("String Utilities")]
+    [ProtocolName("string")]
+    [Order(0)]
+    [NotScrollable]
+    internal sealed class StringUtilitiesToolProvider : ToolProviderBase, IToolProvider
+    {
+        private readonly IMefProvider _mefProvider;
+
+        public string? DisplayName => LanguageManager.Instance.StringUtilities.DisplayName;
+
+        public object IconSource { get; }
+
+        [ImportingConstructor]
+        public StringUtilitiesToolProvider(IMefProvider mefProvider)
+        {
+            _mefProvider = mefProvider;
+
+            IconSource
+                = new TaskCompletionNotifier<IconElement>(
+                    ThreadHelper.RunOnUIThreadAsync(() =>
+                    {
+                        return Task.FromResult<IconElement>(
+                            new FontIcon
+                            {
+                                FontFamily = (FontFamily)Application.Current.Resources["FluentSystemIcons"],
+                                Glyph = "\uF793"
+                            });
+                    }));
+        }
+
+        public bool CanBeTreatedByTool(string data)
+        {
+            return false;
+        }
+
+        public IToolViewModel CreateTool()
+        {
+            return _mefProvider.Import<StringUtilitiesToolViewModel>();
+        }
+    }
+}
---
+++ src/dev/impl/DevToys/ViewModels/Tools/StringUtilities/StringUtilitiesToolViewModel.cs
@@ -0,0 +1,30 @@
+﻿#nullable enable
+
+using DevToys.Api.Tools;
+using DevToys.Views.Tools.StringUtilities;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Composition;
+
+namespace DevToys.ViewModels.Tools.StringUtilities
+{
+    [Export(typeof(StringUtilitiesToolViewModel))]
+    public sealed class StringUtilitiesToolViewModel : ObservableRecipient, IToolViewModel
+    {
+        private string? _text;
+
+        public Type View { get; } = typeof(StringUtilitiesToolPage);
+
+        internal StringUtilitiesStrings Strings => LanguageManager.Instance.StringUtilities;
+
+        internal string? Text
+        {
+            get => _text;
+            set
+            {
+                SetProperty(ref _text, value);
+                // QueueRegExMatch();
+            }
+        }
+    }
+}
