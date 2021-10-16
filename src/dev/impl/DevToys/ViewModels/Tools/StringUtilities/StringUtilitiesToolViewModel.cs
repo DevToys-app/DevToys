e4701ff5612125597ecedea487b92a63fd0271bb
Etienne BAUDOUX <ebaudoux@outlook.com>
2021-10-15 23:18:42 -0700

    Added string converters

--- src/dev/impl/DevToys/ViewModels/Tools/StringUtilities/StringUtilitiesToolViewModel.cs
+++ src/dev/impl/DevToys/ViewModels/Tools/StringUtilities/StringUtilitiesToolViewModel.cs
@@ -4,6 +4,7 @@ using DevToys.Api.Tools;
 using DevToys.Core.Threading;
 using DevToys.Views.Tools.StringUtilities;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Composition;
@@ -17,7 +18,13 @@ namespace DevToys.ViewModels.Tools.StringUtilities
     [Export(typeof(StringUtilitiesToolViewModel))]
     public sealed class StringUtilitiesToolViewModel : ObservableRecipient, IToolViewModel
     {
+        private static readonly object _lockObject = new();
+
+        private bool _forbidBackup;
         private string? _text;
+        private string? _textBackup;
+        private string? _wordDistribution;
+        private string? _characterDistribution;
         private int _selectionStart;
         private int _line;
         private int _column;
@@ -28,8 +35,6 @@ namespace DevToys.ViewModels.Tools.StringUtilities
         private int _sentences;
         private int _paragraphs;
         private int _bytes;
-        private string? _wordDistribution;
-        private string? _characterDistribution;
 
         public Type View { get; } = typeof(StringUtilitiesToolPage);
 
@@ -40,8 +45,15 @@ namespace DevToys.ViewModels.Tools.StringUtilities
             get => _text;
             set
             {
-                SetProperty(ref _text, value);
-                QueueTextStatisticCalculation();
+                lock (_lockObject)
+                {
+                    if (!_forbidBackup)
+                    {
+                        _textBackup = value;
+                    }
+                    SetProperty(ref _text, value);
+                    QueueTextStatisticCalculation();
+                }
             }
         }
 
@@ -124,8 +136,507 @@ namespace DevToys.ViewModels.Tools.StringUtilities
         [ImportingConstructor]
         public StringUtilitiesToolViewModel()
         {
+            SentenceCaseCommand = new RelayCommand(ExecuteSentenceCaseCommand);
+            LowerCaseCommand = new RelayCommand(ExecuteLowerCaseCommand);
+            UpperCaseCommand = new RelayCommand(ExecuteUpperCaseCommand);
+            TitleCaseCommand = new RelayCommand(ExecuteTitleCaseCommand);
+            CamelCaseCommand = new RelayCommand(ExecuteCamelCaseCommand);
+            PascalCaseCommand = new RelayCommand(ExecutePascalCaseCommand);
+            SnakeCaseCommand = new RelayCommand(ExecuteSnakeCaseCommand);
+            ConstantCaseCommand = new RelayCommand(ExecuteConstantCaseCommand);
+            KebabCaseCommand = new RelayCommand(ExecuteKebabCaseCommand);
+            CobolCaseCommand = new RelayCommand(ExecuteCobolCaseCommand);
+            TrainCaseCommand = new RelayCommand(ExecuteTrainCaseCommand);
+            AlternatingCaseCommand = new RelayCommand(ExecuteAlternatingCaseCommand);
+            InverseCaseCommand = new RelayCommand(ExecuteInverseCaseCommand);
+
             QueueSelectionStatisticCalculation();
             QueueTextStatisticCalculation();
+        }
+
+        #region SentenceCaseCommand
+
+        public IRelayCommand SentenceCaseCommand { get; }
+
+        private void ExecuteSentenceCaseCommand()
+        {
+            char[]? sentenceCaseString = _textBackup?.ToCharArray();
+            if (sentenceCaseString is null)
+            {
+                return;
+            }
+
+            bool newSentence = true;
+            for (int i = 0; i < sentenceCaseString.Length; i++)
+            {
+                if (sentenceCaseString[i] == '.'
+                    || sentenceCaseString[i] == '?'
+                    || sentenceCaseString[i] == '!'
+                    || sentenceCaseString[i] == '\r')
+                {
+                    newSentence = true;
+                    continue;
+                }
+
+                if (char.IsLetterOrDigit(sentenceCaseString[i]))
+                {
+                    if (newSentence)
+                    {
+                        sentenceCaseString[i] = char.ToUpperInvariant(sentenceCaseString[i]);
+                        newSentence = false;
+                    }
+                    else
+                    {
+                        sentenceCaseString[i] = char.ToLowerInvariant(sentenceCaseString[i]);
+                    }
+                }
+            }
+
+            lock (_lockObject)
+            {
+                _forbidBackup = true;
+                Text = new string(sentenceCaseString);
+                _forbidBackup = false;
+            }
+        }
+
+        #endregion
+
+        #region LowerCaseCommand
+
+        public IRelayCommand LowerCaseCommand { get; }
+
+        private void ExecuteLowerCaseCommand()
+        {
+            lock (_lockObject)
+            {
+                _forbidBackup = true;
+                Text = _textBackup?.ToLowerInvariant();
+                _forbidBackup = false;
+            }
+        }
+
+        #endregion
+
+        #region UpperCaseCommand
+
+        public IRelayCommand UpperCaseCommand { get; }
+
+        private void ExecuteUpperCaseCommand()
+        {
+            lock (_lockObject)
+            {
+                _forbidBackup = true;
+                Text = _textBackup?.ToUpperInvariant();
+                _forbidBackup = false;
+            }
+        }
+
+        #endregion
+
+        #region TitleCaseCommand
+
+        public IRelayCommand TitleCaseCommand { get; }
+
+        private void ExecuteTitleCaseCommand()
+        {
+            char[]? titleCaseString = _textBackup?.ToCharArray();
+            if (titleCaseString is null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < titleCaseString.Length; i++)
+            {
+                if (i == 0
+                    || !char.IsLetterOrDigit(titleCaseString[i - 1]))
+                {
+                    titleCaseString[i] = char.ToUpperInvariant(titleCaseString[i]);
+                }
+                else
+                {
+                    titleCaseString[i] = char.ToLowerInvariant(titleCaseString[i]);
+                }
+            }
+
+            lock (_lockObject)
+            {
+                _forbidBackup = true;
+                Text = new string(titleCaseString);
+                _forbidBackup = false;
+            }
+        }
+
+        #endregion
+
+        #region CamelCaseCommand
+
+        public IRelayCommand CamelCaseCommand { get; }
+
+        private void ExecuteCamelCaseCommand()
+        {
+            string? text = _textBackup;
+            if (text is null)
+            {
+                return;
+            }
+
+            var camelCaseStringBuilder = new StringBuilder();
+            var nextLetterOrDigitShouldBeUppercase = false;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentChar = text[i];
+                if (char.IsLetterOrDigit(currentChar))
+                {
+                    if (nextLetterOrDigitShouldBeUppercase)
+                    {
+                        camelCaseStringBuilder.Append(char.ToUpperInvariant(currentChar));
+                        nextLetterOrDigitShouldBeUppercase = false;
+                    }
+                    else
+                    {
+                        camelCaseStringBuilder.Append(char.ToLowerInvariant(currentChar));
+                    }
+                }
+                else
+                {
+                    if (currentChar == '\r')
+                    {
+                        nextLetterOrDigitShouldBeUppercase = false;
+                        camelCaseStringBuilder.Append(currentChar);
+                    }
+                    else
+                    {
+                        nextLetterOrDigitShouldBeUppercase = true;
+                    }
+                }
+            }
+
+            lock (_lockObject)
+            {
+                _forbidBackup = true;
+                Text = camelCaseStringBuilder.ToString();
+                _forbidBackup = false;
+            }
+        }
+
+        #endregion
+
+        #region PascalCaseCommand
+
+        public IRelayCommand PascalCaseCommand { get; }
+
+        private void ExecutePascalCaseCommand()
+        {
+            string? text = _textBackup;
+            if (text is null)
+            {
+                return;
+            }
+
+            var pascalCaseStringBuilder = new StringBuilder();
+            var nextLetterOrDigitShouldBeUppercase = true;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentChar = text[i];
+                if (char.IsLetterOrDigit(currentChar))
+                {
+                    if (nextLetterOrDigitShouldBeUppercase)
+                    {
+                        pascalCaseStringBuilder.Append(char.ToUpperInvariant(currentChar));
+                        nextLetterOrDigitShouldBeUppercase = false;
+                    }
+                    else
+                    {
+                        pascalCaseStringBuilder.Append(char.ToLowerInvariant(currentChar));
+                    }
+                }
+                else
+                {
+                    nextLetterOrDigitShouldBeUppercase = true;
+                    if (currentChar == '\r')
+                    {
+                        pascalCaseStringBuilder.Append(currentChar);
+                    }
+                }
+            }
+
+            lock (_lockObject)
+            {
+                _forbidBackup = true;
+                Text = pascalCaseStringBuilder.ToString();
+                _forbidBackup = false;
+            }
+        }
+
+        #endregion
+
+        #region SnakeCaseCommand
+
+        public IRelayCommand SnakeCaseCommand { get; }
+
+        private void ExecuteSnakeCaseCommand()
+        {
+            string? text = _textBackup;
+            if (text is null)
+            {
+                return;
+            }
+
+            var snakeCase = SnakeConstantKebabCobolCaseConverter(text, '_', isUpperCase: false);
+
+            lock (_lockObject)
+            {
+                _forbidBackup = true;
+                Text = snakeCase;
+                _forbidBackup = false;
+            }
+        }
+
+        #endregion
+
+        #region ConstantCaseCommand
+
+        public IRelayCommand ConstantCaseCommand { get; }
+
+        private void ExecuteConstantCaseCommand()
+        {
+            string? text = _textBackup;
+            if (text is null)
+            {
+                return;
+            }
+
+            var constantCase = SnakeConstantKebabCobolCaseConverter(text, '_', isUpperCase: true);
+
+            lock (_lockObject)
+            {
+                _forbidBackup = true;
+                Text = constantCase;
+                _forbidBackup = false;
+            }
+        }
+
+        #endregion
+
+        #region KebabCaseCommand
+
+        public IRelayCommand KebabCaseCommand { get; }
+
+        private void ExecuteKebabCaseCommand()
+        {
+            string? text = _textBackup;
+            if (text is null)
+            {
+                return;
+            }
+
+            var kebabCase = SnakeConstantKebabCobolCaseConverter(text, '-', isUpperCase: false);
+
+            lock (_lockObject)
+            {
+                _forbidBackup = true;
+                Text = kebabCase;
+                _forbidBackup = false;
+            }
+        }
+
+        #endregion
+
+        #region CobolCaseCommand
+
+        public IRelayCommand CobolCaseCommand { get; }
+
+        private void ExecuteCobolCaseCommand()
+        {
+            string? text = _textBackup;
+            if (text is null)
+            {
+                return;
+            }
+
+            var cobolCase = SnakeConstantKebabCobolCaseConverter(text, '-', isUpperCase: true);
+
+            lock (_lockObject)
+            {
+                _forbidBackup = true;
+                Text = cobolCase;
+                _forbidBackup = false;
+            }
+        }
+
+        #endregion
+
+        #region TrainCaseCommand
+
+        public IRelayCommand TrainCaseCommand { get; }
+
+        private void ExecuteTrainCaseCommand()
+        {
+            string? text = _textBackup;
+            if (text is null)
+            {
+                return;
+            }
+
+            var snakeCaseStringBuilder = new StringBuilder();
+
+            var nextNonLetterOrDigitShouldBeIgnored = true;
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentChar = text[i];
+                if (char.IsLetterOrDigit(currentChar))
+                {
+                    if (nextNonLetterOrDigitShouldBeIgnored)
+                    {
+                        snakeCaseStringBuilder.Append(char.ToUpperInvariant(currentChar));
+                    }
+                    else
+                    {
+                        snakeCaseStringBuilder.Append(char.ToLowerInvariant(currentChar));
+                    }
+                    nextNonLetterOrDigitShouldBeIgnored = false;
+                }
+                else if (currentChar == '\r')
+                {
+                    nextNonLetterOrDigitShouldBeIgnored = true;
+                    snakeCaseStringBuilder.Append(currentChar);
+                }
+                else if (!nextNonLetterOrDigitShouldBeIgnored)
+                {
+                    if (i < text.Length - 1
+                        && char.IsLetterOrDigit(text[i + 1]))
+                    {
+                        nextNonLetterOrDigitShouldBeIgnored = true;
+                        snakeCaseStringBuilder.Append('-');
+                    }
+                }
+            }
+
+
+            lock (_lockObject)
+            {
+                _forbidBackup = true;
+                Text = snakeCaseStringBuilder.ToString();
+                _forbidBackup = false;
+            }
+        }
+
+        #endregion
+
+        #region AlternatingCaseCommand
+
+        public IRelayCommand AlternatingCaseCommand { get; }
+
+        private void ExecuteAlternatingCaseCommand()
+        {
+            char[]? titleCaseString = _textBackup?.ToCharArray();
+            if (titleCaseString is null)
+            {
+                return;
+            }
+
+            bool lowerCase = true;
+            for (int i = 0; i < titleCaseString.Length; i++)
+            {
+                if (lowerCase)
+                {
+                    titleCaseString[i] = char.ToLowerInvariant(titleCaseString[i]);
+                }
+                else
+                {
+                    titleCaseString[i] = char.ToUpperInvariant(titleCaseString[i]);
+                }
+
+                lowerCase = !lowerCase;
+            }
+
+            lock (_lockObject)
+            {
+                _forbidBackup = true;
+                Text = new string(titleCaseString);
+                _forbidBackup = false;
+            }
+        }
+
+        #endregion
+
+        #region InverseCaseCommand
+
+        public IRelayCommand InverseCaseCommand { get; }
+
+        private void ExecuteInverseCaseCommand()
+        {
+            char[]? titleCaseString = _textBackup?.ToCharArray();
+            if (titleCaseString is null)
+            {
+                return;
+            }
+
+            bool lowerCase = false;
+            for (int i = 0; i < titleCaseString.Length; i++)
+            {
+                if (lowerCase)
+                {
+                    titleCaseString[i] = char.ToLowerInvariant(titleCaseString[i]);
+                }
+                else
+                {
+                    titleCaseString[i] = char.ToUpperInvariant(titleCaseString[i]);
+                }
+
+                lowerCase = !lowerCase;
+            }
+
+            lock (_lockObject)
+            {
+                _forbidBackup = true;
+                Text = new string(titleCaseString);
+                _forbidBackup = false;
+            }
+        }
+
+        #endregion
+
+        private string SnakeConstantKebabCobolCaseConverter(string text, char spaceReplacement, bool isUpperCase)
+        {
+            var snakeCaseStringBuilder = new StringBuilder();
+
+            var nextNonLetterOrDigitShouldBeIgnored = true;
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentChar = text[i];
+                if (char.IsLetterOrDigit(currentChar))
+                {
+                    nextNonLetterOrDigitShouldBeIgnored = false;
+                    if (isUpperCase)
+                    {
+                        snakeCaseStringBuilder.Append(char.ToUpperInvariant(currentChar));
+                    }
+                    else
+                    {
+                        snakeCaseStringBuilder.Append(char.ToLowerInvariant(currentChar));
+                    }
+                }
+                else if (currentChar == '\r')
+                {
+                    nextNonLetterOrDigitShouldBeIgnored = true;
+                    snakeCaseStringBuilder.Append(currentChar);
+                }
+                else if (!nextNonLetterOrDigitShouldBeIgnored)
+                {
+                    if (i < text.Length - 1
+                        && char.IsLetterOrDigit(text[i + 1]))
+                    {
+                        nextNonLetterOrDigitShouldBeIgnored = true;
+                        snakeCaseStringBuilder.Append(spaceReplacement);
+                    }
+                }
+            }
+
+            return snakeCaseStringBuilder.ToString();
         }
 
         private void QueueTextStatisticCalculation()
@@ -160,12 +671,17 @@ namespace DevToys.ViewModels.Tools.StringUtilities
 
             if (!string.IsNullOrEmpty(text))
             {
+                // Characters
                 characters = text!.Length;
+
+                // Bytes
                 bytes = Encoding.UTF8.GetByteCount(text);
 
+                // Words count
                 MatchCollection matches = Regex.Matches(text, @"\b[\w]*\b");
                 words = matches.Count(m => m.Length > 0);
 
+                // Word distribution
                 var stringBuilder = new StringBuilder();
                 var wordFrequency
                     = matches
@@ -182,6 +698,7 @@ namespace DevToys.ViewModels.Tools.StringUtilities
                 }
                 wordDistribution = stringBuilder.ToString();
 
+                // Paragraphs
                 matches = Regex.Matches(text, @"[^\r\n]*[^ \r\n]+[^\r\n]*((\r|\n|\r\n)[^\r\n]*[^ \r\n]+[^\r\n]*)*");
                 paragraphs = matches.Count(m => m.Length > 0);
 
@@ -228,6 +745,7 @@ namespace DevToys.ViewModels.Tools.StringUtilities
                     sentences++;
                 }
 
+                // Character distributions
                 stringBuilder.Clear();
                 foreach (var item in characterFrequency.OrderByDescending(m => m.Value).ThenBy(m => m.Key))
                 {
