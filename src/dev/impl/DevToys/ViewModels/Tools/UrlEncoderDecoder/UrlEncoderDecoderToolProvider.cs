89d74c03a35bae45dcd9aad0a4a3a8b60ab0022d
Etienne Baudoux <ebaudoux@velersoftware.com>
2021-11-12 19:18:16 -0800

    Added a URL and HTML Encoder/Decoder (#67)
    Co-authored-by: Etienne Baudoux <etbaudou@microsoft.com>

---
+++ src/dev/impl/DevToys/ViewModels/Tools/UrlEncoderDecoder/UrlEncoderDecoderToolProvider.cs
@@ -0,0 +1,49 @@
+﻿#nullable enable
+
+using System.Composition;
+using System.Threading.Tasks;
+using DevToys.Api.Core.Injection;
+using DevToys.Api.Tools;
+using DevToys.Core.Threading;
+using Windows.UI.Xaml.Controls;
+
+namespace DevToys.ViewModels.Tools.UrlEncoderDecoder
+{
+    [Export(typeof(IToolProvider))]
+    [Name("URL Encoder/Decoder")]
+    [ProtocolName("url")]
+    [Order(0)]
+    internal sealed class UrlEncoderDecoderToolProvider : ToolProviderBase, IToolProvider
+    {
+        public string DisplayName => LanguageManager.Instance.UrlEncoderDecoder.DisplayName;
+
+        public object IconSource
+            => new TaskCompletionNotifier<IconElement>(
+                ThreadHelper.RunOnUIThreadAsync(() =>
+                {
+                    return Task.FromResult<IconElement>(
+                        new FontIcon
+                        {
+                            Glyph = "\uF4E4"
+                        });
+                }));
+
+        private readonly IMefProvider _mefProvider;
+
+        [ImportingConstructor]
+        public UrlEncoderDecoderToolProvider(IMefProvider mefProvider)
+        {
+            _mefProvider = mefProvider;
+        }
+
+        public bool CanBeTreatedByTool(string data)
+        {
+            return false;
+        }
+
+        public IToolViewModel CreateTool()
+        {
+            return _mefProvider.Import<UrlEncoderDecoderToolViewModel>();
+        }
+    }
+}
---
+++ src/dev/impl/DevToys/Views/Tools/HtmlEncoderDecoder/HtmlEncoderDecoderToolPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿#nullable enable
+
+using DevToys.Api.Core.Navigation;
+using DevToys.Core;
+using DevToys.ViewModels.Tools.HtmlEncoderDecoder;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace DevToys.Views.Tools.HtmlEncoderDecoder
+{
+    public sealed partial class HtmlEncoderDecoderToolPage : Page
+    {
+        public static readonly DependencyProperty ViewModelProperty
+            = DependencyProperty.Register(
+                nameof(ViewModel),
+                typeof(HtmlEncoderDecoderToolViewModel),
+                typeof(HtmlEncoderDecoderToolPage),
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets the page's view model.
+        /// </summary>
+        public HtmlEncoderDecoderToolViewModel ViewModel
+        {
+            get => (HtmlEncoderDecoderToolViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        public HtmlEncoderDecoderToolPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var parameters = (NavigationParameter)e.Parameter;
+
+            if (ViewModel is null)
+            {
+                // Set the view model
+                Assumes.NotNull(parameters.ViewModel, nameof(parameters.ViewModel));
+                ViewModel = (HtmlEncoderDecoderToolViewModel)parameters.ViewModel!;
+                DataContext = ViewModel;
+            }
+
+            if (!string.IsNullOrWhiteSpace(parameters.ClipBoardContent))
+            {
+                ViewModel.ConversionMode = HtmlEncoderDecoderToolViewModel.DecodeConversion;
+                ViewModel.InputValue = parameters.ClipBoardContent;
+            }
+
+            base.OnNavigatedTo(e);
+        }
+    }
+}
