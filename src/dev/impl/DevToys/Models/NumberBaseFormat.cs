f6fb37eba83c851408290f9e523e4107defe6a31
BenjaminT <titeux.benjamin@outlook.com>
2021-11-29 20:32:53 +0100

    Added hex converter toys (#86)
    * Added hexconverter toy
    
    * added french localization and Accessibility name
    
    * renamed HexConverter to NumberBaseConverter
    
    * cleaned csproj *.resw

---
+++ src/dev/impl/DevToys/Models/NumberBaseFormat.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Globalization;
+
+namespace DevToys.Models
+{
+    public class NumberBaseFormat : IEquatable<NumberBaseFormat>
+    {
+        private static NumberBaseConverterStrings Strings => LanguageManager.Instance.NumberBaseConverter;
+
+        public static readonly NumberBaseFormat Octal = new(
+            displayName: Strings.OctalLabel,
+            value: Radix.Octal,
+            baseNumber:8,
+            groupSize: CultureInfo.CurrentCulture.NumberFormat.NumberGroupSizes[0],
+            groupSeparator: ' ');
+
+        public static readonly NumberBaseFormat Binary = new(
+            displayName: Strings.BinaryLabel,
+            value: Radix.Binary,
+            baseNumber: 2,
+            groupSize: 4,
+            groupSeparator: ' ');
+
+        public static readonly NumberBaseFormat Decimal = new(
+            displayName: Strings.DecimalLabel,
+            value: Radix.Decimal,
+            baseNumber: 10,
+            groupSize: CultureInfo.CurrentCulture.NumberFormat.NumberGroupSizes[0],
+            groupSeparator: Convert.ToChar(CultureInfo.CurrentCulture.NumberFormat.NumberGroupSeparator));
+
+        public static readonly NumberBaseFormat Hexadecimal = new(
+            displayName: Strings.HexadecimalLabel,
+            value: Radix.Hexdecimal,
+            baseNumber: 16,
+            groupSize: 4,
+            groupSeparator: ' ');
+
+        public string DisplayName { get; }
+
+        public Radix Value { get; }
+
+        public int BaseNumber { get; }
+
+        public int GroupSize { get; }
+
+        public char GroupSeparator { get; }
+
+        private NumberBaseFormat(string displayName, Radix value, int baseNumber, int groupSize, char groupSeparator)
+        {
+            DisplayName = displayName;
+            Value = value;
+            BaseNumber = baseNumber;
+            GroupSize = groupSize;
+            GroupSeparator = groupSeparator;
+        }
+
+        public bool Equals(NumberBaseFormat other)
+        {
+            return other.Value == Value;
+        }
+    }
+}
---
+++ src/dev/impl/DevToys/Views/Tools/NumberBaseConverter/NumberBaseConverterToolPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿#nullable enable
+
+using DevToys.Api.Core.Navigation;
+using DevToys.Shared.Core;
+using DevToys.ViewModels.Tools.NumberBaseConverter;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace DevToys.Views.Tools.NumberBaseConverter
+{
+    public sealed partial class NumberBaseConverterToolPage : Page
+    {
+        public static readonly DependencyProperty ViewModelProperty
+            = DependencyProperty.Register(
+                nameof(ViewModel),
+                typeof(NumberBaseConverterToolViewModel),
+                typeof(NumberBaseConverterToolPage),
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets the page's view model.
+        /// </summary>
+        public NumberBaseConverterToolViewModel ViewModel
+        {
+            get => (NumberBaseConverterToolViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        public NumberBaseConverterToolPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (ViewModel is null)
+            {
+                var parameters = (NavigationParameter)e.Parameter;
+
+                // Set the view model
+                Assumes.NotNull(parameters.ViewModel, nameof(parameters.ViewModel));
+                ViewModel = (NumberBaseConverterToolViewModel)parameters.ViewModel!;
+                DataContext = ViewModel;
+            }
+
+            base.OnNavigatedTo(e);
+        }
+    }
+}
