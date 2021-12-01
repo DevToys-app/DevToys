480fbfe8df3fa04e7dc2154fc5293c7123e9d7ed
Etienne BAUDOUX <ebaudoux@velersoftware.com>
2021-11-30 23:25:05 -0800

    Added support of UUID version 1 in GUID Generator tool (#90)

--- src/dev/impl/DevToys/ViewModels/Tools/GuidGenerator/GuidGeneratorToolViewModel.cs
+++ src/dev/impl/DevToys/ViewModels/Tools/GuidGenerator/GuidGeneratorToolViewModel.cs
@@ -17,7 +17,7 @@ namespace DevToys.ViewModels.Tools.GuidGenerator
     public sealed class GuidGeneratorToolViewModel : ObservableRecipient, IToolViewModel
     {
         /// <summary>
-        /// Whether the generated GUID should be uppercase or lowercase.
+        /// Whether the generated UUID should be uppercase or lowercase.
         /// </summary>
         private static readonly SettingDefinition<bool> Uppercase
             = new(
@@ -26,13 +26,22 @@ namespace DevToys.ViewModels.Tools.GuidGenerator
                 defaultValue: false);
 
         /// <summary>
-        /// Whether the generated GUID should have hyphens or not.
+        /// Whether the generated UUID should have hyphens or not.
         /// </summary>
         private static readonly SettingDefinition<bool> Hyphens
             = new(
                 name: $"{nameof(GuidGeneratorToolViewModel)}.{nameof(Hyphens)}",
                 isRoaming: true,
                 defaultValue: true);
+
+        /// <summary>
+        /// The UUID Version to generate.
+        /// </summary>
+        private static readonly SettingDefinition<string> Version
+            = new(
+                name: $"{nameof(GuidGeneratorToolViewModel)}.{nameof(Version)}",
+                isRoaming: true,
+                defaultValue: DefaultVersion);
 
         /// <summary>
         /// How many GUIDs should be generated at once.
@@ -42,6 +51,22 @@ namespace DevToys.ViewModels.Tools.GuidGenerator
                 name: $"{nameof(GuidGeneratorToolViewModel)}.{nameof(GuidsToGenerate)}",
                 isRoaming: true,
                 defaultValue: 1);
+
+        private static readonly Random Random = new Random();
+
+        private static readonly DateTimeOffset GregorianCalendarStart = new(1582, 10, 15, 0, 0, 0, TimeSpan.Zero);
+        private const int VariantByte = 8;
+        private const int VariantByteMask = 0x3f;
+        private const int VariantByteShift = 0x80;
+        private const int VersionByte = 7;
+        private const int VersionByteMask = 0x0f;
+        private const int VersionByteShift = 4;
+        private const int ByteArraySize = 16;
+        private const byte TimestampByte = 0;
+        private const byte NodeByte = 10;
+        private const byte GuidClockSequenceByte = 8;
+        private const string DefaultVersion = "Four";
+        internal const string VersionOne = "One";
 
         private readonly IMarketingService _marketingService;
         private readonly ISettingsProvider _settingsProvider;
@@ -74,6 +99,19 @@ namespace DevToys.ViewModels.Tools.GuidGenerator
                 if (_settingsProvider.GetSetting(Hyphens) != value)
                 {
                     _settingsProvider.SetSetting(Hyphens, value);
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        internal string UuidVersion
+        {
+            get => _settingsProvider.GetSetting(Version);
+            set
+            {
+                if (_settingsProvider.GetSetting(Version) != value)
+                {
+                    _settingsProvider.SetSetting(Version, value);
                     OnPropertyChanged();
                 }
             }
@@ -128,7 +166,16 @@ namespace DevToys.ViewModels.Tools.GuidGenerator
             var newGuids = new StringBuilder();
             for (int i = 0; i < NumberOfGuidsToGenerate; i++)
             {
-                string? guid = Guid.NewGuid().ToString(guidStringFormat);
+                string? guid;
+                if (string.Equals(VersionOne, UuidVersion, StringComparison.Ordinal))
+                {
+                    guid = GenerateTimeBasedGuid().ToString(guidStringFormat);
+                }
+                else
+                {
+                    guid = Guid.NewGuid().ToString(guidStringFormat);
+                }
+
                 if (IsUppercase)
                 {
                     guid = guid.ToUpperInvariant();
@@ -148,5 +195,70 @@ namespace DevToys.ViewModels.Tools.GuidGenerator
         }
 
         #endregion
+
+        private Guid GenerateTimeBasedGuid()
+        {
+            DateTime dateTime = DateTime.UtcNow;
+            long ticks = dateTime.Ticks - GregorianCalendarStart.Ticks;
+
+            byte[] guid = new byte[ByteArraySize];
+            byte[] timestamp = BitConverter.GetBytes(ticks);
+
+            // copy node
+            byte[]? nodes = GenerateNodeBytes();
+            Array.Copy(nodes, 0, guid, NodeByte, Math.Min(6, nodes.Length));
+
+            // copy clock sequence
+            byte[]? clockSequence = GenerateClockSequenceBytes(dateTime);
+            Array.Copy(clockSequence, 0, guid, GuidClockSequenceByte, Math.Min(2, clockSequence.Length));
+
+            // copy timestamp
+            Array.Copy(timestamp, 0, guid, TimestampByte, Math.Min(8, timestamp.Length));
+
+            // set the variant
+            guid[VariantByte] &= (byte)VariantByteMask;
+            guid[VariantByte] |= (byte)VariantByteShift;
+
+            // set the version
+            guid[VersionByte] &= (byte)VersionByteMask;
+            guid[VersionByte] |= (byte)((byte)GuidVersion.TimeBased << VersionByteShift);
+
+            return new Guid(guid);
+        }
+
+        public byte[] GenerateNodeBytes()
+        {
+            byte[]? node = new byte[6];
+
+            Random.NextBytes(node);
+            return node;
+        }
+
+        public byte[] GenerateClockSequenceBytes(DateTime dt)
+        {
+            DateTime utc = dt.ToUniversalTime();
+
+            byte[]? bytes = BitConverter.GetBytes(utc.Ticks);
+
+            if (bytes.Length == 0)
+            {
+                return new byte[] { 0x0, 0x0 };
+            }
+
+            if (bytes.Length == 1)
+            {
+                return new byte[] { 0x0, bytes[0] };
+            }
+
+            return new byte[] { bytes[0], bytes[1] };
+        }
+
+        private enum GuidVersion
+        {
+            TimeBased = 0x01,
+            Reserved = 0x02,
+            NameBased = 0x03,
+            Random = 0x04
+        }
     }
 }
