471da2b04d97ed533504733f653fd5222de4940e
BenjaminT <titeux.benjamin@outlook.com>
2021-12-01 18:23:54 +0100

    added smart detection for Number Base Converter (#91)

---
+++ src/dev/impl/DevToys/Helpers/NumberBaseHelper.cs
@@ -0,0 +1,53 @@
+﻿#nullable enable
+
+using System.Globalization;
+using DevToys.Core.Formatter;
+
+namespace DevToys.Helpers
+{
+    internal static class NumberBaseHelper
+    {
+        /// <summary>
+        /// Detects whether the given string is a valid Hexadecimal Number or not.
+        /// </summary>
+        internal static bool IsValidHexadecimal(string? input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
+            long.TryParse(input, NumberStyles.HexNumber, null, out long output);
+            if (output is not 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Detects whether the given string is a valid Binary Number or not.
+        /// </summary>
+        internal static bool IsValidBinary(string? input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
+            input = input!.Replace("\0", string.Empty);
+
+            foreach (char @char in input!)
+            {
+                if (@char is not '0' and not '1')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
