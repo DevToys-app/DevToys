7a0f37c6f648a4bb6274c95b44f10298e773d0e9
Etienne BAUDOUX <ebaudoux@velersoftware.com>
2021-12-15 18:32:57 -0800

    Made secondary tiles icons dynamically generated. (#118)

---
+++ src/dev/impl/DevToys/Models/TileIconSizeDefinition.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+
+using DevToys.Shared.Core;
+
+namespace DevToys.Models
+{
+    internal sealed class TileIconSizeDefinition
+    {
+        public TileIconSizeDefinition(string iconName, int size, double toolIconRatio)
+        {
+            IconName = Arguments.NotNullOrWhiteSpace(iconName, nameof(iconName));
+            Size = size;
+            ToolIconRatio = toolIconRatio;
+        }
+
+        internal int Size { get; }
+
+        internal double ToolIconRatio { get; }
+
+        internal string IconName { get; }
+    }
+}
