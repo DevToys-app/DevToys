e1dc395bac341435bdbfa7ea07c9d9c39e631362
Sven Vissers <sapvissers@gmail.com>
2022-01-22 21:43:50 +0100

    Applied style guidelines

--- src/dev/impl/DevToys/ViewModels/Tools/Converters/JsonYaml/DoubleConverter.cs
+++ src/dev/impl/DevToys/ViewModels/Tools/Converters/JsonYaml/DoubleConverter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using YamlDotNet.Core;
 using YamlDotNet.Core.Events;
 using YamlDotNet.Serialization;
@@ -7,16 +9,19 @@ namespace DevToys.ViewModels.Tools.JsonYaml
 {
     internal class DoubleConverter : IYamlTypeConverter
     {
-        public bool Accepts(Type type) => type == typeof(double);
+        public bool Accepts(Type type)
+        {
+            return type == typeof(double);
+        }
 
         public object ReadYaml(IParser parser, Type type)
         {
             throw new NotImplementedException();
         }
 
-        public void WriteYaml(IEmitter emitter, object value, Type type)
+        public void WriteYaml(IEmitter emitter, object? value, Type type)
         {
-            string formatted = ((double)value).ToString("r", System.Globalization.CultureInfo.InvariantCulture);
+            string formatted = ((double?)value)?.ToString("r", System.Globalization.CultureInfo.InvariantCulture) ?? "";
             emitter.Emit(new Scalar(AnchorName.Empty, TagName.Empty, formatted, ScalarStyle.Any, true, false));
         }
     }
