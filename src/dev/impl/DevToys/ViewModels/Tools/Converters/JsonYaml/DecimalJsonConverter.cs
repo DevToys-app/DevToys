e26bffd32a0f7c18bdfe06162547c86b375f9133
akarboush <akarboush@gmail.com>
2022-01-24 18:47:52 +0100

    Fixes numbers are converted to string in Yaml to Json conversion

---
+++ src/dev/impl/DevToys/ViewModels/Tools/Converters/JsonYaml/DecimalJsonConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DevToys.ViewModels.Tools.Converters.JsonYaml
+{
+    internal class DecimalJsonConverter : JsonConverter<decimal>
+    {
+        public override decimal ReadJson(JsonReader reader, Type objectType, decimal existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, decimal value, JsonSerializer serializer)
+        {
+            // prevent adding trailing zeros 
+            writer.WriteRawValue(value.ToString());
+        }
+    }
+}
---
+++ src/dev/impl/DevToys/ViewModels/Tools/Converters/JsonYaml/DecimalYamlTypeResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+
+namespace DevToys.ViewModels.Tools.Converters.JsonYaml
+{
+    internal class DecimalYamlTypeResolver : INodeTypeResolver
+    {
+        public bool Resolve(NodeEvent? nodeEvent, ref Type currentType)
+        {
+            if (nodeEvent is Scalar scalar)
+            {
+                // avoid unnecessary parsing attempts
+                bool couldBeNumber =
+                    scalar.Value.Length != 0 &&
+                    scalar.Value[0] is >= '0' and <= '9' ||
+                    scalar.Value[0] == '-';
+
+                if (couldBeNumber && decimal.TryParse(scalar.Value, out _))
+                {
+                    currentType = typeof(decimal);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
