f8a796e9127bb533944cc6a8dab6f5f5b6e01850
Jorge Machado <youkai.jamt95@gmail.com>
2022-01-24 09:37:23 -0500

    Fixing hex validation. "0" was not an accepted hex value (#222)
    Co-authored-by: Youkai <Youkai@DESKTOP-897VL99>

--- src/dev/impl/DevToys/Helpers/NumberBaseHelper.cs
+++ src/dev/impl/DevToys/Helpers/NumberBaseHelper.cs
@@ -16,13 +16,7 @@ namespace DevToys.Helpers
                 return false;
             }
 
-            long.TryParse(input, NumberStyles.HexNumber, null, out long output);
-            if (output is not 0)
-            {
-                return true;
-            }
-
-            return false;
+            return long.TryParse(input, NumberStyles.HexNumber, null, out _);
         }
 
         /// <summary>
