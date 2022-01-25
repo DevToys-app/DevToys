d118b0dc6327398314aa6e5e4db88aa40fd24623
Jorge Machado <youkai.jamt95@gmail.com>
2022-01-25 17:58:57 -0500

    Fixed typo in exception handler (#253)
    * Fixing hex validation. "0" was not an accepted hex value
    
    * Typo in exception logger
    
    Co-authored-by: Youkai <Youkai@DESKTOP-897VL99>

--- src/dev/impl/DevToys/ViewModels/Tools/Converters/JsonYaml/JsonYamlToolViewModel.cs
+++ src/dev/impl/DevToys/ViewModels/Tools/Converters/JsonYaml/JsonYamlToolViewModel.cs
@@ -264,7 +264,7 @@ namespace DevToys.ViewModels.Tools.JsonYaml
             }
             catch (Exception ex)
             {
-                Logger.LogFault("Yaml to Json Converter", ex);
+                Logger.LogFault("Json to Yaml Converter", ex);
                 output = string.Empty;
             }
 
