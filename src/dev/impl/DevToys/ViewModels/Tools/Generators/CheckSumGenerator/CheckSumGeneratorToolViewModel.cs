faf1ddefb6138a26526d7baa959b38513c138b81
Joaolfelicio <joaolfelicio@gmail.com>
2022-01-27 19:30:25 +0100

    Make ComputetationTaskQueue readonly

--- src/dev/impl/DevToys/ViewModels/Tools/Generators/CheckSumGenerator/CheckSumGeneratorToolViewModel.cs
+++ src/dev/impl/DevToys/ViewModels/Tools/Generators/CheckSumGenerator/CheckSumGeneratorToolViewModel.cs
@@ -66,7 +66,7 @@ namespace DevToys.ViewModels.Tools.CheckSumGenerator
         private bool _hasCancelledCalculation;
         private bool _shouldDisplayProgress;
         private HashingAlgorithmDisplayPair? _outputHashingAlgorithm;
-        private Queue<Task> _computationTaskQueue = new();
+        private readonly Queue<Task> _computationTaskQueue = new();
 
         internal CheckSumGeneratorStrings Strings => LanguageManager.Instance.CheckSumGenerator;
 
