cf46e08f1b6124cdf1426d197f876c3d574d6f07
Etienne BAUDOUX <ebaudoux@velersoftware.com>
2022-01-29 22:42:22 -0800

    Added a new reusable FileSelector control allowing tools to get one or many files as input.

--- src/dev/impl/DevToys/ViewModels/Tools/Generators/CheckSumGenerator/CheckSumGeneratorToolViewModel.cs
+++ src/dev/impl/DevToys/ViewModels/Tools/Generators/CheckSumGenerator/CheckSumGeneratorToolViewModel.cs
@@ -4,28 +4,25 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Composition;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
+using System.Threading;
 using System.Threading.Tasks;
 using DevToys.Api.Core;
 using DevToys.Api.Core.Settings;
 using DevToys.Api.Tools;
+using DevToys.Core;
 using DevToys.Core.Threading;
+using DevToys.Helpers;
 using DevToys.Models;
-using DevToys.Shared.Core;
+using DevToys.Shared.Core.Threading;
 using DevToys.Views.Tools.CheckSumGenerator;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage;
-using DevToys.Shared.Core.Threading;
-using Windows.Storage.Pickers;
-using Windows.UI.Xaml;
-using System.Linq;
-using DevToys.Core;
-using Clipboard = Windows.ApplicationModel.DataTransfer.Clipboard;
-using DevToys.Helpers;
-using System.Threading;
 
 namespace DevToys.ViewModels.Tools.CheckSumGenerator
 {
@@ -56,7 +53,6 @@ namespace DevToys.ViewModels.Tools.CheckSumGenerator
 
         private readonly object _lockObject = new();
         private CancellationTokenSource _cancellationTokenSource = new();
-        private bool _isSelectFilesAreaHighlighted;
         private bool _isCalculationInProgress;
         private IStorageFile? _inputFile;
         private int _progress;
@@ -127,12 +123,6 @@ namespace DevToys.ViewModels.Tools.CheckSumGenerator
             }
         }
 
-        internal bool IsSelectFilesAreaHighlighted
-        {
-            get => _isSelectFilesAreaHighlighted;
-            set => SetProperty(ref _isSelectFilesAreaHighlighted, value);
-        }
-
         internal bool ShouldDisplayProgress
         {
             get => _shouldDisplayProgress;
@@ -168,107 +158,21 @@ namespace DevToys.ViewModels.Tools.CheckSumGenerator
         {
             _settingsProvider = settingsProvider;
             _marketingService = marketingService;
-            SelectFilesAreaDragOverCommand = new RelayCommand<DragEventArgs>(ExecuteSelectFilesAreaDragOverCommand);
-            SelectFilesAreaDragLeaveCommand = new RelayCommand<DragEventArgs>(ExecuteSelectFilesAreaDragLeaveCommand);
-            SelectFilesAreaDragDropCommand = new AsyncRelayCommand<DragEventArgs>(ExecuteSelectFilesAreaDragDropCommandAsync);
-            SelectFilesBrowseCommand = new AsyncRelayCommand(ExecuteSelectFilesBrowseCommandAsync);
-            SelectFilesPasteCommand = new AsyncRelayCommand(ExecuteSelectFilesPasteCommandAsync);
+            FilesSelectedCommand = new RelayCommand<StorageFile[]>(ExecuteFilesSelectedCommand);
             CancelCommand = new RelayCommand(() => ExecuteCancelCommand(shouldRevertHashingAlgo: true));
         }
 
-        #region SelectFilesAreaDragOverCommand
+        #region FilesSelectedCommand
 
-        public IRelayCommand<DragEventArgs> SelectFilesAreaDragOverCommand { get; }
+        public IRelayCommand<StorageFile[]> FilesSelectedCommand { get; }
 
-        private void ExecuteSelectFilesAreaDragOverCommand(DragEventArgs? parameters)
+        private void ExecuteFilesSelectedCommand(StorageFile[]? files)
         {
-            Arguments.NotNull(parameters, nameof(parameters));
-
-            if (parameters!.DataView.Contains(StandardDataFormats.StorageItems))
+            if (files is not null)
             {
-                parameters.AcceptedOperation = DataPackageOperation.Copy;
-                parameters.Handled = false;
+                Debug.Assert(files.Length == 1);
+                InputFile = files[0];
             }
-
-            IsSelectFilesAreaHighlighted = true;
-        }
-
-        #endregion
-
-        #region SelectFilesAreaDragLeaveCommand
-
-        public IRelayCommand<DragEventArgs> SelectFilesAreaDragLeaveCommand { get; }
-
-        private void ExecuteSelectFilesAreaDragLeaveCommand(DragEventArgs? parameters)
-        {
-            IsSelectFilesAreaHighlighted = false;
-        }
-
-        #endregion
-
-        #region SelectFilesAreaDragDropCommand
-
-        public IAsyncRelayCommand<DragEventArgs> SelectFilesAreaDragDropCommand { get; }
-
-        private async Task ExecuteSelectFilesAreaDragDropCommandAsync(DragEventArgs? parameters)
-        {
-            Arguments.NotNull(parameters, nameof(parameters));
-
-            await ThreadHelper.RunOnUIThreadAsync(async () =>
-            {
-                IsSelectFilesAreaHighlighted = false;
-
-                IStorageItem? storageItem = await ExtractStorageItem(parameters!.DataView);
-                if (storageItem is StorageFile file)
-                {
-                    InputFile = file;
-                }
-            }).ConfigureAwait(false);
-        }
-
-        #endregion
-
-        #region SelectFilesBrowseCommand
-
-        public IAsyncRelayCommand SelectFilesBrowseCommand { get; }
-
-        private async Task ExecuteSelectFilesBrowseCommandAsync()
-        {
-            var filePicker = new FileOpenPicker
-            {
-                ViewMode = PickerViewMode.List,
-                SuggestedStartLocation = PickerLocationId.ComputerFolder,
-            };
-            filePicker.FileTypeFilter.Add("*");
-
-            await ThreadHelper.RunOnUIThreadAsync(async () =>
-            {
-                StorageFile file = await filePicker.PickSingleFileAsync();
-                if (file != null)
-                {
-                    InputFile = file;
-                }
-            });
-        }
-
-        #endregion
-
-        #region SelectFilesPasteCommand
-
-        public IAsyncRelayCommand SelectFilesPasteCommand { get; }
-
-        private async Task ExecuteSelectFilesPasteCommandAsync()
-        {
-            await ThreadHelper.RunOnUIThreadAsync(async () =>
-            {
-                DataPackageView? dataPackageView = Clipboard.GetContent();
-
-                IStorageItem? storageItem = await ExtractStorageItem(dataPackageView);
-                if (storageItem is StorageFile file)
-                {
-                    InputFile = file;
-                }
-            }).ConfigureAwait(false);
         }
 
         #endregion
@@ -282,7 +186,7 @@ namespace DevToys.ViewModels.Tools.CheckSumGenerator
             lock (_lockObject)
             {
                 _hasCancelledCalculation = true;
-                
+
                 _cancellationTokenSource.Cancel();
                 _cancellationTokenSource.Dispose();
                 _cancellationTokenSource = new CancellationTokenSource();
