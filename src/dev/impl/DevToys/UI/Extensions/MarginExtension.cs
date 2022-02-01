47ccc8320b654e08070d0e35d945a8ee8ac9597c
BenjaminT <titeux.benjamin@outlook.com>
2022-02-01 22:51:57 +0100

    added MarkupExtension to align button on some toys

---
+++ src/dev/impl/DevToys/UI/Extensions/MarginExtension.cs
@@ -0,0 +1,89 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Markup;
+
+namespace DevToys.UI.Extensions
+{
+    /// <summary>
+    /// Based on https://gist.github.com/angularsen/90040fb174f71c5ab3ad
+    /// </summary>
+    [Bindable]
+    public sealed class MarginExtension : MarkupExtension
+    {
+        public static readonly DependencyProperty MarginProperty
+            = DependencyProperty.RegisterAttached(
+                "Margin",
+                typeof(Thickness),
+                typeof(MarginExtension),
+                new PropertyMetadata(new Thickness(), MarginChangedCallback));
+
+        public static readonly DependencyProperty LastItemMarginProperty
+            = DependencyProperty.RegisterAttached(
+                "LastItemMargin",
+                typeof(Thickness),
+                typeof(MarginExtension),
+                new PropertyMetadata(new Thickness(), MarginChangedCallback));
+
+        public static Thickness GetMargin(DependencyObject obj)
+        {
+            return (Thickness)obj.GetValue(MarginProperty);
+        }
+
+        public static void SetLastItemMargin(DependencyObject obj, Thickness value)
+        {
+            obj.SetValue(LastItemMarginProperty, value);
+        }
+
+        public static void SetMargin(DependencyObject obj, Thickness value)
+        {
+            obj.SetValue(MarginProperty, value);
+        }
+
+        private static Thickness GetLastItemMargin(Panel obj)
+        {
+            return (Thickness)obj.GetValue(LastItemMarginProperty);
+        }
+
+        private static void MarginChangedCallback(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Make sure this is put on a panel
+            if (sender is not Panel panel)
+            {
+                return;
+            }
+
+            // Avoid duplicate registrations
+            panel.Loaded -= OnPanelLoaded;
+            panel.Loaded += OnPanelLoaded;
+
+            if (panel.IsLoaded)
+            {
+                OnPanelLoaded(panel, null);
+            }
+        }
+
+        private static void OnPanelLoaded(object sender, RoutedEventArgs e)
+        {
+            var panel = (Panel)sender;
+
+            // Go over the children and set margin for them:
+            IEnumerable<UIElement> visibleChildren = panel.Children.Where(elem => elem.Visibility != Visibility.Collapsed);
+            for (int i = 0; i < visibleChildren.Count(); i++)
+            {
+                UIElement child = panel.Children[i];
+                if (child is not FrameworkElement fe)
+                {
+                    continue;
+                }
+
+                bool isLastItem = i == visibleChildren.Count() - 1;
+                fe.Margin = isLastItem ? GetLastItemMargin(panel) : GetMargin(panel);
+            }
+        }
+    }
+}
---
+++ src/dev/impl/DevToys/UI/Extensions/SpacingExtension.cs
@@ -0,0 +1,68 @@
+﻿#nullable enable
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Markup;
+
+namespace DevToys.UI.Extensions
+{
+    /// <summary>
+    /// Based on https://gist.github.com/angularsen/90040fb174f71c5ab3ad
+    /// </summary>
+    [Bindable]
+    public sealed class SpacingExtension : MarkupExtension
+    {
+
+        public static readonly DependencyProperty VerticalProperty 
+            = DependencyProperty.RegisterAttached(
+                "Vertical", 
+                typeof(double), 
+                typeof(SpacingExtension),
+                new PropertyMetadata(0d, VerticalChangedCallback));
+
+        public static readonly DependencyProperty HorizontalProperty 
+            = DependencyProperty.RegisterAttached(
+                "Horizontal", 
+                typeof(double), 
+                typeof(SpacingExtension),
+                new PropertyMetadata(0d, HorizontalChangedCallback));
+
+        public static double GetHorizontal(DependencyObject obj)
+        {
+            return (double)obj.GetValue(HorizontalProperty);
+        }
+
+        public static void SetHorizontal(DependencyObject obj, double space)
+        {
+            obj.SetValue(HorizontalProperty, space);
+        }
+
+        public static double GetVertical(DependencyObject obj)
+        {
+            return (double)obj.GetValue(VerticalProperty);
+        }
+
+        public static void SetVertical(DependencyObject obj, double value)
+        {
+            obj.SetValue(VerticalProperty, value);
+        }
+
+        private static void HorizontalChangedCallback(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            double space = (double)e.NewValue;
+            var obj = (DependencyObject)sender;
+
+            MarginExtension.SetMargin(obj, new Thickness(0, 0, space, 0));
+            MarginExtension.SetLastItemMargin(obj, new Thickness(0));
+        }
+
+        private static void VerticalChangedCallback(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            double space = (double)e.NewValue;
+            var obj = (DependencyObject)sender;
+            MarginExtension.SetMargin(obj, new Thickness(0, 0, 0, space));
+            MarginExtension.SetLastItemMargin(obj, new Thickness(0));
+        }
+
+    }
+}
