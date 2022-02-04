75502f264e2420d726551d8419b38d12584b6db4
Rafael López <rlm96@hotmail.com>
2022-02-04 01:52:57 +0100

    Fixes to pass Chocolatey review

--- chocolatey/DevToys/tools/chocolateyinstall.ps1
+++ chocolatey/DevToys/tools/chocolateyinstall.ps1
@@ -4,8 +4,9 @@ $url64      = 'https://github.com/veler/DevToys/releases/download/v1.0.2.0/64360
 $checksum64 = '287e0fe143b25069df2d43eb8fd7dedeff9abc6f3fe7799ccb218a180d2068a3'
 $WindowsVersion  = [Environment]::OSVersion.Version;
 $InstallDir = Split-Path $MyInvocation.MyCommand.Definition;
+$AppxPackageName = "64360VelerSoftware.DevToys"
 
-if ($WindowsVersion.Major -ne "10") {
+if ($WindowsVersion.Major -lt "10") {
   throw "This package requires Windows 10.";
 };
 
@@ -15,32 +16,26 @@ if ($IsCorrectBuild -lt "18362") {
   throw "This package requires at least Windows 10 version 1903/OS build 18362.x.";
 };
 
-$packageArgs = @{
+$PackageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'MSI'
   url64bit      = $url64
 
   softwareName  = 'DevToys'
 
-  checksumType  = 'sha256'
   checksum64    = $checksum64
   checksumType64= 'sha256'
-
-  # MSI
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
 
   fileFullPath = Join-Path $InstallDir $([IO.Path]::GetFileName($Url64))
 }
 
 Get-ChocolateyWebFile @PackageArgs
 
-if ((Get-AppxPackage -name $PackageArgs.packageName).Version -Match $Version) {
+if ((Get-AppxPackage -name $AppxPackageName).Version -Match $Version) {
   if($env:ChocolateyForce) {
-    # you can't install the same version of an appx package, you need to remove it first
+    #You can't install the same version of an appx package, you need to remove it first
     Write-Host Removing already installed version first. ;
-    Get-AppxPackage -Name $PackageArgs.packageName | Remove-AppxPackage;
+    Get-AppxPackage -Name $AppxPackageName | Remove-AppxPackage;
   } else {
     Write-Host The $Version version of the package is already installed. If you want to reinstall use --force ;
     return;
--- chocolatey/DevToys/update.ps1
+++ chocolatey/DevToys/update.ps1
@@ -21,4 +21,4 @@ function global:au_SearchReplace {
     }
 }
 
-update -ChecksumFor 64 
+update -ChecksumFor 64 #-Force
