7d9b4d2e86561ac9efdd8f1e174fc17d6668c0d4
youkai95 <youkai.jamt95@gmail.com>
2022-03-05 18:44:09 -0500

    Add new communication messages between number converter view models

---
+++ src/dev/impl/DevToys/Messages/ChangeNumberFormattingMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevToys.Messages
+{
+    public sealed class ChangeNumberFormattingMessage
+    {
+        public bool IsFormatted { get; set; }
+
+        public ChangeNumberFormattingMessage(bool formatted)
+        {
+            IsFormatted = formatted;
+        }
+    }
+}
