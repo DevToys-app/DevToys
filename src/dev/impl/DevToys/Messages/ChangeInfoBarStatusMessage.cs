583d6bcddcbc2953eea9ef3ed40d1adb26c6e385
youkai95 <youkai.jamt95@gmail.com>
2022-03-05 18:44:09 -0500

    Add new communication messages between number converter view models

---
+++ src/dev/impl/DevToys/Messages/ChangeInfoBarStatusMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevToys.Messages
+{
+    public sealed class ChangeInfoBarStatusMessage
+    {
+        public string Message { get; set; }
+
+        public ChangeInfoBarStatusMessage(string message)
+        {
+            Message = message;
+        }
+    }
+}
