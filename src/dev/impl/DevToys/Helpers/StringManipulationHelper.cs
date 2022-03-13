cf0bdfb1fea4dc82f301f76c441f4d7822b53d21
Etienne BAUDOUX <ebaudoux@velersoftware.com>
2022-03-13 14:08:04 -0700

    Added String Escape / Unescape tool

---
+++ src/dev/impl/DevToys/Helpers/StringManipulationHelper.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+
+namespace DevToys.Helpers
+{
+    internal static class StringManipulationHelper
+    {
+        internal static bool HasEscapeCharacters(string data)
+        {
+            return data.Contains("\\n")
+                || data.Contains("\\r")
+                || data.Contains("\\\\")
+                || data.Contains("\\\"")
+                || data.Contains("\\t")
+                || data.Contains("\\f")
+                || data.Contains("\\b");
+        }
+    }
+}
---
+++ src/dev/impl/DevToys/Views/Tools/Text/StringEscapeUnescape/StringEscapeUnescapeToolPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿#nullable enable
+
+using DevToys.Api.Core.Navigation;
+using DevToys.Helpers;
+using DevToys.Shared.Core;
+using DevToys.ViewModels.Tools.StringEscapeUnescape;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DevToys.Views.Tools.StringEscapeUnescape
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class StringEscapeUnescapeToolPage : Page
+    {
+        public static readonly DependencyProperty ViewModelProperty
+            = DependencyProperty.Register(
+                nameof(ViewModel),
+                typeof(StringEscapeUnescapeToolViewModel),
+                typeof(StringEscapeUnescapeToolPage),
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets the page's view model.
+        /// </summary>
+        public StringEscapeUnescapeToolViewModel ViewModel
+        {
+            get => (StringEscapeUnescapeToolViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        public StringEscapeUnescapeToolPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var parameters = (NavigationParameter)e.Parameter;
+
+            if (ViewModel is null)
+            {
+                // Set the view model
+                Assumes.NotNull(parameters.ViewModel, nameof(parameters.ViewModel));
+                ViewModel = (StringEscapeUnescapeToolViewModel)parameters.ViewModel!;
+                DataContext = ViewModel;
+            }
+
+            if (!string.IsNullOrWhiteSpace(parameters.ClipBoardContent))
+            {
+                ViewModel.IsEscapeMode = !StringManipulationHelper.HasEscapeCharacters(parameters.ClipBoardContent!);
+                ViewModel.InputValue = parameters.ClipBoardContent;
+            }
+
+            base.OnNavigatedTo(e);
+        }
+    }
+}
