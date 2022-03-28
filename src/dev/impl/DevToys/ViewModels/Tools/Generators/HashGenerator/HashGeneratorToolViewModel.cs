0bef79efb82e268e0fcb2713dd250f2b2c13363e
Etienne Baudoux <ebaudoux@velersoftware.com>
2022-03-27 23:00:37 -0700

    Improved unit tests reliability

--- src/dev/impl/DevToys/ViewModels/Tools/Generators/HashGenerator/HashGeneratorToolViewModel.cs
+++ src/dev/impl/DevToys/ViewModels/Tools/Generators/HashGenerator/HashGeneratorToolViewModel.cs
@@ -1,7 +1,6 @@
 ﻿#nullable enable
 
 using System;
-using System.Collections.Generic;
 using System.Composition;
 using System.Threading.Tasks;
 using DevToys.Api.Core;
@@ -11,7 +10,6 @@ using DevToys.Core;
 using DevToys.Core.Threading;
 using DevToys.Shared.Core.Threading;
 using DevToys.Views.Tools.HashGenerator;
-using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Windows.Security.Cryptography;
 using Windows.Security.Cryptography.Core;
 using Windows.Storage.Streams;
@@ -19,7 +17,7 @@ using Windows.Storage.Streams;
 namespace DevToys.ViewModels.Tools.HashGenerator
 {
     [Export(typeof(HashGeneratorToolViewModel))]
-    public sealed class HashGeneratorToolViewModel : ObservableRecipient, IToolViewModel
+    public sealed class HashGeneratorToolViewModel : QueueWorkerViewModelBase<string>, IToolViewModel
     {
         /// <summary>
         /// Whether the generated hash should be uppercase or lowercase.
@@ -41,10 +39,8 @@ namespace DevToys.ViewModels.Tools.HashGenerator
 
         private readonly IMarketingService _marketingService;
         private readonly ISettingsProvider _settingsProvider;
-        private readonly Queue<string> _hashCalculationQueue = new();
 
         private bool _toolSuccessfullyWorked;
-        private bool _calculationInProgress;
         private string? _input;
         private string? _md5;
         private string? _sha1;
@@ -129,46 +125,31 @@ namespace DevToys.ViewModels.Tools.HashGenerator
 
         private void QueueHashCalculation()
         {
-            _hashCalculationQueue.Enqueue(Input ?? string.Empty);
-            TreatQueueAsync().Forget();
+            EnqueueComputation(Input ?? string.Empty);
         }
 
-        private async Task TreatQueueAsync()
+        protected override async Task TreatComputationQueueAsync(string value)
         {
-            if (_calculationInProgress)
+            Task<string> md5CalculationTask = CalculateHashAsync(HashAlgorithmNames.Md5, value);
+            Task<string> sha1CalculationTask = CalculateHashAsync(HashAlgorithmNames.Sha1, value);
+            Task<string> sha256CalculationTask = CalculateHashAsync(HashAlgorithmNames.Sha256, value);
+            Task<string> sha512CalculationTask = CalculateHashAsync(HashAlgorithmNames.Sha512, value);
+
+            await Task.WhenAll(md5CalculationTask).ConfigureAwait(false);
+
+            await ThreadHelper.RunOnUIThreadAsync(() =>
             {
-                return;
-            }
+                MD5 = md5CalculationTask.Result;
+                SHA1 = sha1CalculationTask.Result;
+                SHA256 = sha256CalculationTask.Result;
+                SHA512 = sha512CalculationTask.Result;
 
-            _calculationInProgress = true;
-
-            await TaskScheduler.Default;
-
-            while (_hashCalculationQueue.TryDequeue(out string? text))
-            {
-                Task<string> md5CalculationTask = CalculateHashAsync(HashAlgorithmNames.Md5, text);
-                Task<string> sha1CalculationTask = CalculateHashAsync(HashAlgorithmNames.Sha1, text);
-                Task<string> sha256CalculationTask = CalculateHashAsync(HashAlgorithmNames.Sha256, text);
-                Task<string> sha512CalculationTask = CalculateHashAsync(HashAlgorithmNames.Sha512, text);
-
-                await Task.WhenAll(md5CalculationTask).ConfigureAwait(false);
-
-                ThreadHelper.RunOnUIThreadAsync(() =>
+                if (!_toolSuccessfullyWorked)
                 {
-                    MD5 = md5CalculationTask.Result;
-                    SHA1 = sha1CalculationTask.Result;
-                    SHA256 = sha256CalculationTask.Result;
-                    SHA512 = sha512CalculationTask.Result;
-
-                    if (!_toolSuccessfullyWorked)
-                    {
-                        _toolSuccessfullyWorked = true;
-                        _marketingService.NotifyToolSuccessfullyWorked();
-                    }
-                }).ForgetSafely();
-            }
-
-            _calculationInProgress = false;
+                    _toolSuccessfullyWorked = true;
+                    _marketingService.NotifyToolSuccessfullyWorked();
+                }
+            });
         }
 
         private async Task<string> CalculateHashAsync(string alrogithmName, string text)
@@ -187,7 +168,7 @@ namespace DevToys.ViewModels.Tools.HashGenerator
                 IBuffer buffer = CryptographicBuffer.ConvertStringToBinary(text, BinaryStringEncoding.Utf8);
                 buffer = algorithmProvider.HashData(buffer);
 
-                string? hash="";
+                string? hash = "";
                 if (string.Equals(OutputType, HexOutput))
                 {
                     hash = IsUppercase
