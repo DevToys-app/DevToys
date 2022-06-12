032006240a00b3b9679afdf4838b35104f74c89a
davidTigris <davidTigris 45315755+davidTigris@users.noreply.github.com>
2022-06-12 16:01:51 +0200

    feat: use standard info dialog

--- src/dev/impl/DevToys/ViewModels/Tools/Text/XmlValidator/XmlValidatorToolViewModel.cs
+++ src/dev/impl/DevToys/ViewModels/Tools/Text/XmlValidator/XmlValidatorToolViewModel.cs
@@ -1,40 +1,128 @@
 ﻿using System;
 using System.Composition;
+using System.Xml.Schema;
 using DevToys.Api.Core.Settings;
 using DevToys.Api.Tools;
+using DevToys.Core.Threading;
+using DevToys.Models;
+using DevToys.ViewModels.Tools.XmlValidator.Parsing;
 using DevToys.Views.Tools.XmlValidator;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml.Controls;
 
 namespace DevToys.ViewModels.Tools.XmlValidator
 {
     [Export(typeof(XmlValidatorToolViewModel))]
     public class XmlValidatorToolViewModel : ObservableRecipient, IToolViewModel
     {
-        private string? _xsdSchema;
+        private string? _xsdSchemaData;
         private string? _xmlData;
-
-        public Type View { get; } = typeof(XmlValidatorToolPage);
+        private XmlParsingResult? _parsedXml;
+        private XsdParsingResult? _parsedXsdScheme;
+        private readonly XmlValidatorStrings _localizedStrings;
+        private InfoBarData? _validationResult;
 
         internal ISettingsProvider SettingsProvider { get; }
-
         internal XmlValidatorStrings Strings => LanguageManager.Instance.XmlValidator;
 
         internal string? XsdSchema
         {
-            get => _xsdSchema;
-            set => SetProperty(ref _xsdSchema, value, broadcast: true);
+            get => _xsdSchemaData;
+            set
+            {
+                ThreadHelper.ThrowIfNotOnUIThread();
+                SetProperty(ref _xsdSchemaData, value);
+                ProcessNewXsdData();
+            }
         }
-
+        
         internal string? XmlData
         {
             get => _xmlData;
-            set => SetProperty(ref _xmlData, value, broadcast: true);
+            set
+            {
+                ThreadHelper.ThrowIfNotOnUIThread();
+                SetProperty(ref _xmlData, value, broadcast: true);
+                ProcessNewXmlData();
+            }
         }
 
+        public InfoBarData? ValidationResult
+        {
+            get => _validationResult;
+            private set => SetProperty(ref _validationResult, value);
+        }
+        
+        public Type View { get; } = typeof(XmlValidatorToolPage);
+        
         [ImportingConstructor]
         public XmlValidatorToolViewModel(ISettingsProvider settingsProvider)
         {
             SettingsProvider = settingsProvider;
+            _localizedStrings = new XmlValidatorStrings();
+        }
+
+        private void ProcessNewXmlData()
+        {
+            if (string.IsNullOrWhiteSpace(_xmlData))
+            {
+                _parsedXml = null;
+                DisplayValidationResult();
+                return;
+            }
+            
+            XmlSchemaSet schemaSet = _parsedXsdScheme?.SchemaSet ?? new XmlSchemaSet();
+            XmlParser xmlParser = new("XML data", schemaSet);
+            _parsedXml = xmlParser.Parse(_xmlData ?? String.Empty);
+            
+            DisplayValidationResult();
+        }
+
+        private void ProcessNewXsdData()
+        {
+            if (string.IsNullOrWhiteSpace(_xsdSchemaData))
+            {
+                _parsedXsdScheme = null;
+                DisplayValidationResult();
+                return;
+            }
+            
+            XsdParser xsdParser = new("XSD data");
+            _parsedXsdScheme = xsdParser.Parse(_xsdSchemaData ?? string.Empty);
+            
+            DisplayValidationResult();
+        }
+
+        private void DisplayValidationResult()
+        {
+            if (_parsedXsdScheme is null || _parsedXml is null)
+            {
+                string validationImpossibleMsg = _localizedStrings.ValidationImpossibleMsg;
+                ValidationResult = new InfoBarData(InfoBarSeverity.Informational, validationImpossibleMsg);
+                return;
+            }
+            
+            InfoBarSeverity infoBarSeverity;
+            string message = _parsedXml.ErrorMessage + Environment.NewLine + _parsedXsdScheme.ErrorMessage;
+            
+            bool wasValidationPerformedWithoutErrors = string.IsNullOrEmpty(_parsedXml.ErrorMessage) &&
+                                                       string.IsNullOrEmpty(_parsedXml.ErrorMessage);
+            
+            if (!_parsedXml.IsValid || !_parsedXml.IsValid)
+            {
+                infoBarSeverity = InfoBarSeverity.Error;
+            }
+            else if (wasValidationPerformedWithoutErrors is not true)
+            {
+                infoBarSeverity = InfoBarSeverity.Warning;
+            }
+            else
+            {
+                infoBarSeverity= InfoBarSeverity.Success;
+                message = _localizedStrings.XmlIsValidMessage;
+            }
+
+            ValidationResult = new InfoBarData(infoBarSeverity, message);
         }
     }
 }
--- src/dev/impl/DevToys/Views/Tools/Text/XmlValidator/XmlValidatorToolPage.xaml.cs
+++ src/dev/impl/DevToys/Views/Tools/Text/XmlValidator/XmlValidatorToolPage.xaml.cs
@@ -1,14 +1,11 @@
 ﻿#nullable enable
 
-using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using DevToys.Api.Core.Navigation;
 using DevToys.Shared.Core;
 using DevToys.ViewModels.Tools.XmlValidator;
-using DevToys.ViewModels.Tools.XmlValidator.Parsing;
 
 namespace DevToys.Views.Tools.XmlValidator
 {
@@ -20,9 +17,6 @@ namespace DevToys.Views.Tools.XmlValidator
                 typeof(XmlValidatorToolViewModel),
                 typeof(XmlValidatorToolPage),
                 new PropertyMetadata(null));
-
-        private bool _xmlIsValid;
-        private readonly XmlValidatorStrings _localizedStrings;
 
         /// <summary>
         /// Gets the page's view model.
@@ -36,7 +30,6 @@ namespace DevToys.Views.Tools.XmlValidator
         public XmlValidatorToolPage()
         {
             InitializeComponent();
-            _localizedStrings = new XmlValidatorStrings();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -52,59 +45,6 @@ namespace DevToys.Views.Tools.XmlValidator
             }
 
             base.OnNavigatedTo(e);
-        }
-
-        private void ValidateButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(ViewModel.XmlData) || string.IsNullOrEmpty(ViewModel.XsdSchema))
-            {
-                string errorMessage = _localizedStrings.ValidationImpossibleMsg;
-                DisplayValidationResult(false, errorMessage);
-
-                return;
-            }
-
-            ResetGui();
-
-            XsdParser xsdParser = new("XSD content");
-            XsdParsingResult xsdParsingResult = xsdParser.Parse(ViewModel.XsdSchema!);
-            if (!xsdParsingResult.IsValid)
-            {
-                DisplayValidationResult(false, xsdParsingResult.ErrorMessage!);
-                return;
-            }
-
-            XmlParser xmlParser = new XmlParser("XML content", xsdParsingResult.SchemaSet!);
-            XmlParsingResult xmlParsingResult = xmlParser.Parse(ViewModel.XmlData!);
-            if (!xmlParsingResult.IsValid)
-            {
-                DisplayValidationResult(false, xmlParsingResult.ErrorMessage!);
-                return;
-            }
-
-            _xmlIsValid = true;
-            DisplayValidationResult(_xmlIsValid, _localizedStrings.XmlIsValidMessage);
-        }
-
-        private void ResetGui()
-        {
-            _xmlIsValid = false;
-            ValidationResult.Visibility = Visibility.Collapsed;
-            ValidationMessages.Text = string.Empty;
-        }
-
-        private void DisplayValidationResult(bool validationSucceeded, string validationMessages)
-        {
-            string iconSourcePath = "../../../../Assets/Icons/CheckMark.svg";
-            if (!validationSucceeded)
-            {
-                iconSourcePath = "../../../../Assets/Icons/ExclamationMark.svg";
-            }
-
-            var iconUri = new Uri(BaseUri, iconSourcePath);
-            ResultIcon.Source = new SvgImageSource(iconUri);
-            ValidationMessages.Text = validationMessages;
-            ValidationResult.Visibility = Visibility.Visible;
         }
     }
 }
