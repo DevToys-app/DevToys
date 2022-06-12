0d24817c587295e35834ca956404541282732c60
davidTigris <davidTigris 45315755+davidTigris@users.noreply.github.com>
2022-06-12 12:02:18 +0200

    fix: remove 'validate' button

--- src/dev/impl/DevToys/LanguageManager.cs
+++ src/dev/impl/DevToys/LanguageManager.cs
@@ -3023,11 +3023,6 @@ namespace DevToys
         public string SearchKeywords => _resources.GetString("SearchKeywords");
 
         /// <summary>
-        /// Gets the resource Validate.
-        /// </summary>
-        public string Validate => _resources.GetString("Validate");
-
-        /// <summary>
         /// Gets the resource ValidationImpossibleMsg.
         /// </summary>
         public string ValidationImpossibleMsg => _resources.GetString("ValidationImpossibleMsg");
