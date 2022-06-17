46aff9d8ac0e603613d6b55263c1394eaa533f1c
davidTigris <davidTigris 45315755+davidTigris@users.noreply.github.com>
2022-06-17 17:17:53 +0200

    feat: support SmartDetection

--- src/dev/impl/DevToys/ViewModels/Tools/Text/XmlValidator/XmlValidatorToolProvider.cs
+++ src/dev/impl/DevToys/ViewModels/Tools/Text/XmlValidator/XmlValidatorToolProvider.cs
@@ -1,6 +1,11 @@
 ﻿# nullable enable
 
+using System;
 using System.Composition;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
 using Windows.UI.Xaml.Controls;
 using DevToys.Api.Tools;
 using DevToys.Core.Threading;
@@ -38,7 +43,37 @@ namespace DevToys.ViewModels.Tools.XmlValidator
 
         public bool CanBeTreatedByTool(string data)
         {
-            return false;
+            return ValidateXmlData(data) || ValidateXsdData(data);
+        }
+
+        private bool ValidateXmlData(string data)
+        {
+            try
+            {
+                _ = XDocument.Parse(data);
+            }
+            catch (XmlException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        
+        private bool ValidateXsdData(string data)
+        {
+            bool isXsdValid = true;
+            try
+            {
+                _ = XmlSchema.Read(new XmlTextReader(new StringReader(data)), (_, _) => isXsdValid = false);
+            }
+            catch (Exception e) when (e is XmlException or XmlSchemaException)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return isXsdValid;
         }
 
         public IToolViewModel CreateTool()
