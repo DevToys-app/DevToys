94a20eceb8cac81067423436f64c6161796b64d3
Gabriel <45315755+JustAGabriel@users.noreply.github.com>
2022-06-19 20:58:40 +0200

    feat: add xml validation tool (#525)
    * feat: add xml validation tool
    
    The introduced tool allows to validate XML data against a XSD schema.
    
    Refs: #171
    
    * fix: revert manifest changes
    
    * fix: remove 'validate' button
    
    * feat: use standard info dialog
    
    * chore: delete unused assets
    
    * refactor: delete unused code
    
    * feat: add new tool icon
    
    * feat: add 'enable nullable' directive
    
    * feat: adjust accessibility levels
    
    * refactor: apply code convention
    
    * feat: display info bar at startup
    
    * fix: adjust overlay view layout
    
    * feat: support SmartDetection
    
    * fix: validation execution
    
    * fix: remove unnecessary localization
    
    Co-authored-by: davidTigris <45315755+davidTigris@users.noreply.github.com>
    Co-authored-by: davidTigris <davidTigris 45315755+davidTigris@users.noreply.github.com>

---
+++ src/dev/impl/DevToys/ViewModels/Tools/Text/XmlValidator/Parsing/XmlParser.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace DevToys.ViewModels.Tools.XmlValidator.Parsing
+{
+    internal class XmlParser : ParserBase<XmlParsingResult>
+    {
+        private readonly XmlSchemaSet _xsdSchema;
+
+        internal XmlParser(string source, XmlSchemaSet xsdSchema) : base(source)
+        {
+            _xsdSchema = xsdSchema;
+        }
+
+        internal override XmlParsingResult Parse(string content)
+        {
+            XDocument xmlFile = new();
+            try
+            {
+                xmlFile = XDocument.Parse(content, LoadOptions.SetLineInfo);
+            }
+            catch (XmlException ex)
+            {
+                string formattedErrorMsg = FormatErrorMessage(Source, ex.LineNumber, ex.LinePosition, ex.Message);
+                return new XmlParsingResult {IsValid = false, ErrorMessage = formattedErrorMsg};
+            }
+
+
+            xmlFile.Validate(_xsdSchema, ValidationErrorCallBack);
+            if (HasValidationSucceeded is false)
+            {
+                return new XmlParsingResult {IsValid = false, ErrorMessage = GetValidationMessages()};
+            }
+
+            return new XmlParsingResult {IsValid = true, XmlDocument = xmlFile};
+        }
+    }
+}
---
+++ src/dev/impl/DevToys/Views/Tools/Text/XmlValidator/XmlValidatorToolPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿#nullable enable
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using DevToys.Api.Core.Navigation;
+using DevToys.Shared.Core;
+using DevToys.ViewModels.Tools.XmlValidator;
+
+namespace DevToys.Views.Tools.XmlValidator
+{
+    public sealed partial class XmlValidatorToolPage : Page
+    {
+        public static readonly DependencyProperty ViewModelProperty
+            = DependencyProperty.Register(
+                nameof(ViewModel),
+                typeof(XmlValidatorToolViewModel),
+                typeof(XmlValidatorToolPage),
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets the page's view model.
+        /// </summary>
+        public XmlValidatorToolViewModel ViewModel
+        {
+            get => (XmlValidatorToolViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        public XmlValidatorToolPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (ViewModel is null)
+            {
+                var parameters = (NavigationParameter)e.Parameter;
+
+                // Set the view model
+                Assumes.NotNull(parameters.ViewModel, nameof(parameters.ViewModel));
+                ViewModel = (XmlValidatorToolViewModel)parameters.ViewModel!;
+                DataContext = ViewModel;
+            }
+
+            base.OnNavigatedTo(e);
+        }
+    }
+}
