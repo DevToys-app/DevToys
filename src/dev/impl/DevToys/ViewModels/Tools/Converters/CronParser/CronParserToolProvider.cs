7c324bd05cacddefec4d921d5c9df1f98682b955
Nerijus <nerijus@dotnetinaction.co.uk>
2022-07-08 06:59:26 +0100

    feat: add cron parser tool (#535)
    * initial cron generator
    
    * CRON parser update - initial loading, added Cronos reference, UI and localization
    
    * translation and cleanup
    
    * output format validation
    better text
    icons
    
    * replaced input field with text box
    
    * Final commit, icon, some spelling and last fixes
    
    * new icon
    
    * PR comments - compact mode fixes and text box autofill
    
    * Addressed feedback
    
    * Addressed more feedback
    
    Co-authored-by: Nerijus Stimbirys <nerijus.stimbirys@uk.aswatson.com>
    Co-authored-by: Etienne BAUDOUX <ebaudoux@velersoftware.com>

---
+++ src/dev/impl/DevToys/ViewModels/Tools/Converters/CronParser/CronParserToolProvider.cs
@@ -0,0 +1,48 @@
+﻿#nullable enable
+
+using System.Composition;
+using DevToys.Api.Tools;
+using DevToys.Core.Threading;
+using DevToys.Shared.Api.Core;
+using Windows.UI.Xaml.Controls;
+
+namespace DevToys.ViewModels.Tools.CronParser
+{
+    [Export(typeof(IToolProvider))]
+    [Name("CronParser")]
+    [Parent(ConvertersGroupToolProvider.InternalName)]
+    [ProtocolName("cronparser")]
+    [Order(4)]
+    internal sealed class CronParserToolProvider : ToolProviderBase, IToolProvider
+    {
+        private readonly IMefProvider _mefProvider;
+
+        public string MenuDisplayName => LanguageManager.Instance.CRONParser.MenuDisplayName;
+
+        public string? SearchDisplayName => LanguageManager.Instance.CRONParser.SearchDisplayName;
+
+        public string? Description => LanguageManager.Instance.CRONParser.Description;
+
+        public string AccessibleName => LanguageManager.Instance.CRONParser.AccessibleName;
+
+        public string? SearchKeywords => LanguageManager.Instance.CRONParser.SearchKeywords;
+
+        public TaskCompletionNotifier<IconElement> IconSource => CreateSvgIcon("Cron.svg");
+
+        [ImportingConstructor]
+        public CronParserToolProvider(IMefProvider mefProvider)
+        {
+            _mefProvider = mefProvider;
+        }
+
+        public bool CanBeTreatedByTool(string data)
+        {
+            return false;
+        }
+
+        public IToolViewModel CreateTool()
+        {
+            return _mefProvider.Import<CronParserToolViewModel>();
+        }
+    }
+}
