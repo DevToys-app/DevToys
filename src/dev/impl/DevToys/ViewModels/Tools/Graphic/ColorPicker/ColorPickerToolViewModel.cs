b3ab0eeaba0f75b0e90ef8658b8dd2a044bddab8
Etienne Baudoux <ebaudoux@velersoftware.com>
2022-07-16 18:37:28 -0700

    Added contrast ratio checker (#594)

--- src/dev/impl/DevToys/ViewModels/Tools/Graphic/ColorPicker/ColorPickerToolViewModel.cs
+++ src/dev/impl/DevToys/ViewModels/Tools/Graphic/ColorPicker/ColorPickerToolViewModel.cs
@@ -30,7 +30,12 @@ namespace DevToys.ViewModels.Tools.Graphic.ColorPicker
                 defaultValue: ColorPickerModeDisplayPair.HSV.Value);
 
         private readonly ISettingsProvider _settingsProvider;
-        private Color _selectedColor;
+        private Color _textColor;
+        private Color _backgroundColor;
+        private bool _wcagAaLevelLargeText;
+        private bool _wcagAaLevelSmallText;
+        private bool _wcagAaaLevelLargeText;
+        private bool _wcagAaaLevelSmallText;
 
         public Type View => typeof(ColorPickerToolPage);
 
@@ -70,10 +75,63 @@ namespace DevToys.ViewModels.Tools.Graphic.ColorPicker
         /// <summary>
         /// Gets or sets the selected color.
         /// </summary>
-        public Color SelectedColor
+        public Color TextColor
         {
-            get => _selectedColor;
-            set => SetProperty(ref _selectedColor, value);
+            get => _textColor;
+            set
+            {
+                SetProperty(ref _textColor, value);
+                CalculateContrastRatio();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the selected color.
+        /// </summary>
+        public Color BackgroundColor
+        {
+            get => _backgroundColor;
+            set
+            {
+                SetProperty(ref _backgroundColor, value);
+                CalculateContrastRatio();
+            }
+        }
+
+        /// <summary>
+        /// Gets whether the color contrast pass the WCAG AA level for large text.
+        /// </summary>
+        public bool WcagAaLevelLargeText
+        {
+            get => _wcagAaLevelLargeText;
+            private set => SetProperty(ref _wcagAaLevelLargeText, value);
+        }
+
+        /// <summary>
+        /// Gets whether the color contrast pass the WCAG AA level for small text.
+        /// </summary>
+        public bool WcagAaLevelSmallText
+        {
+            get => _wcagAaLevelSmallText;
+            private set => SetProperty(ref _wcagAaLevelSmallText, value);
+        }
+
+        /// <summary>
+        /// Gets whether the color contrast pass the WCAG AAA level for large text.
+        /// </summary>
+        public bool WcagAaaLevelLargeText
+        {
+            get => _wcagAaaLevelLargeText;
+            private set => SetProperty(ref _wcagAaaLevelLargeText, value);
+        }
+
+        /// <summary>
+        /// Gets whether the color contrast pass the WCAG AAA level for small text.
+        /// </summary>
+        public bool WcagAaaLevelSmallText
+        {
+            get => _wcagAaaLevelSmallText;
+            private set => SetProperty(ref _wcagAaaLevelSmallText, value);
         }
 
         [ImportingConstructor]
@@ -82,7 +140,41 @@ namespace DevToys.ViewModels.Tools.Graphic.ColorPicker
             _settingsProvider = settingsProvider;
 
             string? accentColorResourceName = themeListener.ActualAppTheme == ApplicationTheme.Light ? "SystemAccentColorLight2" : "SystemAccentColorDark1";
-            SelectedColor = (Color)Application.Current.Resources[accentColorResourceName];
+            BackgroundColor = (Color)Application.Current.Resources[accentColorResourceName];
+            TextColor = themeListener.ActualAppTheme == ApplicationTheme.Light ? Colors.Black : Colors.White;
+        }
+
+        private void CalculateContrastRatio()
+        {
+            double textLuminence = CalculateLuminence(TextColor);
+            double backgroundLuminence = CalculateLuminence(BackgroundColor);
+
+            double contrastRatio
+                = textLuminence > backgroundLuminence
+                ? ((backgroundLuminence + 0.05) / (textLuminence + 0.05))
+                : ((textLuminence + 0.05) / (backgroundLuminence + 0.05));
+
+            WcagAaLevelLargeText = contrastRatio < 1 / 3f;
+            WcagAaLevelSmallText = contrastRatio < 1 / 4.5;
+            WcagAaaLevelLargeText = contrastRatio < 1 / 4.5;
+            WcagAaaLevelSmallText = contrastRatio < 1 / 7f;
+        }
+
+        private double CalculateLuminence(Color color)
+        {
+            double r = Luminence(color.R);
+            double g = Luminence(color.G);
+            double b = Luminence(color.B);
+
+            return r * 0.2126 + g * 0.7152 + b * 0.0722;
+
+            static double Luminence(double value)
+            {
+                value /= 255;
+                return value < 0.03928
+                    ? value / 12.92
+                    : Math.Pow((value + 0.055) / 1.055, 2.4);
+            }
         }
     }
 }
