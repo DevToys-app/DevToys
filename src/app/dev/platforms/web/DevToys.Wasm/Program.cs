2441fde68a498cfa00b8b33948a35870264277be
Etienne BAUDOUX <ebaudoux@velersoftware.com>
2023-01-04 20:20:51 -0800

    Added Wasm project (failing to start)

---
+++ src/app/dev/platforms/web/DevToys.Wasm/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DevToys.Wasm;
+
+public class Program
+{
+    private static App? app;
+
+    private static int Main(string[] args)
+    {
+        Microsoft.UI.Xaml.Application.Start(_ => app = new App());
+
+        return 0;
+    }
+}
