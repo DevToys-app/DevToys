58a0752d4c5a0f30741194d7fa6a55399cd57e8c
Etienne BAUDOUX <ebaudoux@velersoftware.com>
2023-02-03 23:43:56 -0800

    Added basic MonacoEditor to MAUI Blazor

---
+++ src/app/dev/platforms/desktop/DevToys.MauiBlazor/Components/MonacoEditor.razor.js
@@ -0,0 +1,52 @@
+﻿// Initialize Monaco Editor
+export function monacoInitialize(id, dotNetHelper, options) {
+    // require is provided by loader.min.js.
+    require.config({
+        paths: { 'vs': options.path }
+    });
+
+    require(["vs/editor/editor.main"], () => {
+        var container = document.getElementById(id);
+
+        // Hide the Progress Ring
+        monaco.editor.onDidCreateEditor((e) => {
+            var progress = document.getElementById(id + "-loading");
+            if (progress && progress.style) {
+                progress.style.display = "none";
+            }
+        });
+
+        // Create the Monaco Editor
+        container.editor = monaco.editor.create(container, {
+            ariaLabel: "online code editor",
+            value: options.value,
+            language: options.language,
+            theme: options.theme,
+            lineNumbers: options.lineNumbers ? "on" : "off",
+            readOnly: options.readOnly,
+        });
+
+        // Keep a reference of dotNetHelper
+        container.dotNetHelper = dotNetHelper;
+
+        // Catch when the editor lost the focus (didType to immediate)
+        container.editor.onDidBlurEditorText((e) => {
+            var code = container.editor.getValue();
+            container.dotNetHelper.invokeMethodAsync("UpdateValueAsync", code);
+        });
+
+        container.editor.updateOptions(options);
+
+        window.editor = container.editor;   // To debug
+    });
+}
+
+// Update the editor options
+export function monacoSetOptions(id, options) {
+    var container = document.getElementById(id);
+    container.editor.setValue(options.value);
+    container.editor.updateOptions({
+        language: options.language,
+        theme: options.theme
+    });
+}
