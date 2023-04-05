62d3110c47959b0f1926e250147645de31dd5da7
Jesse Levier <jlevier@gmail.com>
2023-04-05 15:59:35 -0400

    Adding url encoding wrapper function to handle xaml textbox conversion of new line to only carriage return

---
+++ src/tests/DevToys.Tests/Helpers/UrlHelperTests.cs
@@ -0,0 +1,18 @@
+﻿using DevToys.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevToys.Tests.Helpers
+{
+    [TestClass]
+    public class UrlHelperTests
+    {
+        [DataTestMethod]
+        [DataRow("123\r456", "123%0A456")]
+        [DataRow("test\r", "test%0A")]
+        [DataRow("hello\r\nworld", "hello%0Aworld")]
+        public void EncodeNewLines(string input, string expectedResult)
+        {
+            Assert.AreEqual(expectedResult, UrlHelper.UrlEncode(input));
+        }
+    }
+}
