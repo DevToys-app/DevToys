7eb5b214157ad53e9798ccd4f6fb8080c6670d5e
Etienne Baudoux <ebaudoux@velersoftware.com>
2023-06-04 09:02:37 -0700

    Added unit tests

--- src/app/tests/DevToys.UnitTests/Api/Core/ObservableHashSetTests.cs
+++ src/app/tests/DevToys.UnitTests/Api/Core/ObservableHashSetTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.ObjectModel;
-using System.Collections.Specialized;
+﻿using System.Collections.Specialized;
 using System.ComponentModel;
 
 namespace DevToys.UnitTests.Api.Core;
