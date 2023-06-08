3f4cd7d40768bde5809a5c1e8300310c6db908ad
Etienne BAUDOUX <ebaudoux@velersoftware.com>
2023-06-07 17:20:59 -0700

    [DevToys 2.0] Added CheckBox and ToggleSwitch (#841)
    * [DevToys 2.0] Added context menu and popup system.
    
    * Fixed bug in TextBox involving Clipboard usage
    
    * Remove duplicates in CSS file.
    
    * Removed some duplicated code
    
    * [DevToys 2.0] Added search bar + keyboard navigation in context menu
    
    * [DevToys 2.0] Added CheckBox and ToggleSwitch
    
    * tiny fix
    
    * Addressed feedback

---
+++ src/app/dev/DevToys.Blazor/Components/CheckBox/CheckBox.razor.cs
@@ -0,0 +1,37 @@
+﻿namespace DevToys.Blazor.Components;
+
+public partial class CheckBox : JSStyledComponentBase
+{
+    /// <summary>
+    /// Gets or sets the text to display in the check box.
+    /// </summary>
+    [Parameter]
+    public string? Text { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether the check box is checked.
+    /// </summary>
+    [Parameter]
+    public bool IsChecked { get; set; }
+
+    /// <summary>
+    /// Raised when the check box is toggled.
+    /// </summary>
+    [Parameter]
+    public EventCallback<bool> OnCheckedChanged { get; set; }
+
+    internal ValueTask<bool> FocusAsync()
+    {
+        return JSRuntime.InvokeVoidWithErrorHandlingAsync("devtoys.DOM.setFocus", Element);
+    }
+
+    private void OnChange(ChangeEventArgs ev)
+    {
+        bool value = (ev.Value as bool?) ?? false;
+        if (value != IsChecked)
+        {
+            IsChecked = value;
+            OnCheckedChanged.InvokeAsync(value);
+        }
+    }
+}
---
+++ src/app/dev/DevToys.Blazor/Components/ToggleSwitch/ToggleSwitch.razor.cs
@@ -0,0 +1,37 @@
+﻿namespace DevToys.Blazor.Components;
+
+public partial class ToggleSwitch : JSStyledComponentBase
+{
+    /// <summary>
+    /// Gets or sets the text to display in the toggle switch.
+    /// </summary>
+    [Parameter]
+    public string? Text { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether the toggle switch is on.
+    /// </summary>
+    [Parameter]
+    public bool IsOn { get; set; }
+
+    /// <summary>
+    /// Raised when the toggle switch is toggled.
+    /// </summary>
+    [Parameter]
+    public EventCallback<bool> OnCheckedChanged { get; set; }
+
+    internal ValueTask<bool> FocusAsync()
+    {
+        return JSRuntime.InvokeVoidWithErrorHandlingAsync("devtoys.DOM.setFocus", Element);
+    }
+
+    private void OnChange(ChangeEventArgs ev)
+    {
+        bool value = (ev.Value as bool?) ?? false;
+        if (value != IsOn)
+        {
+            IsOn = value;
+            OnCheckedChanged.InvokeAsync(value);
+        }
+    }
+}
