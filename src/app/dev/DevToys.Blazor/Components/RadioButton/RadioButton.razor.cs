b2f03f7eea5b40c3a1c90793ddb192801f5ffc36
Etienne Baudoux <ebaudoux@velersoftware.com>
2023-06-17 13:28:38 -0700

    Added radio button

---
+++ src/app/dev/DevToys.Blazor/Components/RadioButton/RadioButton.razor.cs
@@ -0,0 +1,43 @@
+﻿namespace DevToys.Blazor.Components;
+
+public partial class RadioButton : JSStyledComponentBase
+{
+    /// <summary>
+    /// Gets or sets the text to display in the radio button.
+    /// </summary>
+    [Parameter]
+    public string? Text { get; set; }
+
+    /// <summary>
+    /// Gets or sets the text to display in the check box.
+    /// </summary>
+    [Parameter]
+    public string? Group { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether the radio button is checked.
+    /// </summary>
+    [Parameter]
+    public bool IsChecked { get; set; }
+
+    /// <summary>
+    /// Raised when the radio button is toggled.
+    /// </summary>
+    [Parameter]
+    public EventCallback<bool> IsCheckedChanged { get; set; }
+
+    internal ValueTask<bool> FocusAsync()
+    {
+        return JSRuntime.InvokeVoidWithErrorHandlingAsync("devtoys.DOM.setFocus", Element);
+    }
+
+    private void OnChange(ChangeEventArgs ev)
+    {
+        bool value = (ev.Value as bool?) ?? false;
+        if (value != IsChecked)
+        {
+            IsChecked = value;
+            IsCheckedChanged.InvokeAsync(value);
+        }
+    }
+}
