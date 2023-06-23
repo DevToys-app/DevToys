b78d77a92dbe65cdb9c7ae1ce31416dddb251f27
Etienne Baudoux <ebaudoux@velersoftware.com>
2023-06-22 22:46:57 -0700

    Added InfoBar

---
+++ src/app/dev/DevToys.Blazor/Components/Feedback/InfoBar/InfoBar.razor.cs
@@ -0,0 +1,62 @@
+﻿namespace DevToys.Blazor.Components;
+
+public partial class InfoBar : StyledComponentBase
+{
+    public InfoBar()
+    {
+        VerticalAlignment = VerticalAlignment.Center;
+    }
+
+    [Parameter]
+    public bool IsOpen { get; set; }
+
+    [Parameter]
+    public bool IsClosable { get; set; } = true;
+
+    [Parameter]
+    public bool IsIconVisible { get; set; } = true;
+
+    [Parameter]
+    public string Title { get; set; } = string.Empty;
+
+    [Parameter]
+    public string Message { get; set; } = string.Empty;
+
+    [Parameter]
+    public string ActionButtonText { get; set; } = string.Empty;
+
+    [Parameter]
+    public bool IsActionButtonAccent { get; set; }
+
+    [Parameter]
+    public InfoBarSeverity Severity { get; set; } = InfoBarSeverity.Informational;
+
+    [Parameter]
+    public EventCallback Closed { get; set; }
+
+    [Parameter]
+    public EventCallback ActionButtonClicked { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+    }
+
+    private void OnCloseButtonClicked()
+    {
+        IsOpen = false;
+        if (Closed.HasDelegate)
+        {
+            Closed.InvokeAsync();
+        }
+    }
+
+    private void OnActionButtonClicked()
+    {
+        if (ActionButtonClicked.HasDelegate)
+        {
+            ActionButtonClicked.InvokeAsync();
+        }
+        OnCloseButtonClicked();
+    }
+}
---
+++ src/app/dev/DevToys.Blazor/Components/Feedback/InfoBar/InfoBarSeverity.cs
@@ -0,0 +1,17 @@
+﻿namespace DevToys.Blazor.Components;
+
+public sealed class InfoBarSeverity
+{
+    internal static readonly InfoBarSeverity Error = new("error");
+    internal static readonly InfoBarSeverity Informational = new("informational");
+    internal static readonly InfoBarSeverity Success = new("success");
+    internal static readonly InfoBarSeverity Warning = new("warning");
+
+    private InfoBarSeverity(string className)
+    {
+        Guard.IsNotNullOrWhiteSpace(className);
+        Class = className;
+    }
+
+    public string Class { get; }
+}
