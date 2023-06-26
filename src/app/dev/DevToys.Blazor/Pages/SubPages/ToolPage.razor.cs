706fb2c8806a3f4b7e369b0f977e43d1cd026a64
Etienne Baudoux <ebaudoux@velersoftware.com>
2023-06-25 20:04:30 -0700

    Added tool navigation

--- src/app/dev/DevToys.Blazor/Pages/SubPages/ToolPage.razor.cs
+++ src/app/dev/DevToys.Blazor/Pages/SubPages/ToolPage.razor.cs
@@ -1,5 +1,24 @@
-﻿namespace DevToys.Blazor.Pages.SubPages;
+﻿using DevToys.Blazor.Components;
+using DevToys.Business.ViewModels;
+using DevToys.Core.Tools.ViewItems;
 
-public partial class ToolPage
+namespace DevToys.Blazor.Pages.SubPages;
+
+public partial class ToolPage : MefComponentBase
 {
+    [Import]
+    internal ToolPageViewModel ViewModel { get; set; } = default!;
+
+    [Parameter]
+    public GuiToolViewItem? GuiToolViewItem { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        if (GuiToolViewItem is not null)
+        {
+            ViewModel.Load(GuiToolViewItem);
+        }
+    }
 }
