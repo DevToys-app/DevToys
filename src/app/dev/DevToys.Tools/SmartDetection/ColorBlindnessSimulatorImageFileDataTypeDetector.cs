151f1cbe37447bc7cc411afe5013ca9be108f689
Etienne Baudoux <ebaudoux@velersoftware.com>
2023-11-05 11:10:21 -0800

    Ported Color Blindness SImulator to DevToys 2.0

---
+++ src/app/dev/DevToys.Tools/SmartDetection/ColorBlindnessSimulatorImageFileDataTypeDetector.cs
@@ -0,0 +1,32 @@
+﻿namespace DevToys.Tools.SmartDetection;
+
+[Export(typeof(IDataTypeDetector))]
+[DataTypeName("ColorBlindnessSimulatorImageFile", baseName: PredefinedCommonDataTypeNames.File)]
+internal sealed partial class ColorBlindnessSimulatorImageFileDataTypeDetector : IDataTypeDetector
+{
+    internal static readonly string[] SupportedFileTypes = new[]
+    {
+        ".bmp",
+        ".jpeg",
+        ".jpg",
+        ".pbm",
+        ".png",
+        ".tiff",
+        ".tga",
+        ".webp"
+    };
+
+    public ValueTask<DataDetectionResult> TryDetectDataAsync(object data, DataDetectionResult? resultFromBaseDetector, CancellationToken cancellationToken)
+    {
+        if (resultFromBaseDetector is not null
+            && resultFromBaseDetector.Data is FileInfo dataFile)
+        {
+            if (SupportedFileTypes.Contains(dataFile.Extension, StringComparer.OrdinalIgnoreCase))
+            {
+                return ValueTask.FromResult(new DataDetectionResult(Success: true, Data: dataFile));
+            }
+        }
+
+        return ValueTask.FromResult(DataDetectionResult.Unsuccessful);
+    }
+}
