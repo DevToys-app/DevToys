045b794ac59521b97f4300b1ce68eeb3d06fdd31
Etienne Baudoux <ebaudoux@velersoftware.com>
2023-12-14 21:19:41 -0800

    Added unit tests

---
+++ src/app/tests/DevToys.UnitTests/Tools/Helpers/UuidHelperTests.cs
@@ -0,0 +1,43 @@
+﻿using DevToys.Tools.Helpers;
+using DevToys.Tools.Models;
+
+namespace DevToys.UnitTests.Tools.Helpers;
+
+public class UuidHelperTests
+{
+    [Fact]
+    internal void GenerateUuidVersionOne()
+    {
+        TestUuid(UuidVersion.One, hyphens: true, uppercase: true);
+        TestUuid(UuidVersion.One, hyphens: false, uppercase: true);
+        TestUuid(UuidVersion.One, hyphens: true, uppercase: false);
+        TestUuid(UuidVersion.One, hyphens: false, uppercase: false);
+    }
+
+    [Fact]
+    internal void GenerateUuidVersionFour()
+    {
+        TestUuid(UuidVersion.Four, hyphens: true, uppercase: true);
+        TestUuid(UuidVersion.Four, hyphens: false, uppercase: true);
+        TestUuid(UuidVersion.Four, hyphens: true, uppercase: false);
+        TestUuid(UuidVersion.Four, hyphens: false, uppercase: false);
+    }
+
+    private void TestUuid(UuidVersion uuidVersion, bool hyphens, bool uppercase)
+    {
+        string newUuid
+            = UuidHelper.GenerateUuid(
+                uuidVersion,
+                hyphens,
+                uppercase);
+        TestUuid(newUuid, hyphens, uppercase);
+    }
+
+    private void TestUuid(string uuid, bool hyphens, bool uppercase)
+    {
+        Guid.TryParse(uuid, out _).Should().BeTrue();
+        uuid.Contains("-").Should().Be(hyphens);
+        uuid.Length.Should().Be(hyphens ? 36 : 32);
+        uuid.Should().Be(uppercase ? uuid.ToUpperInvariant() : uuid.ToLowerInvariant());
+    }
+}
