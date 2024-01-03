80a226c596dce85b367005878f7691f14c1d2d69
Etienne BAUDOUX <ebaudoux@velersoftware.com>
2024-01-02 19:32:14 -0800

    Enabled Hover capability on Monaco Editor and fixed JSON serialization issue. (#1015)

---
+++ src/app/dev/DevToys.Blazor/Core/Converters/PolymorphicConverterFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DevToys.Blazor.Core.Converters;
+
+internal sealed class PolymorphicConverterFactory : JsonConverterFactory
+{
+    /// <inheritdoc />
+    public override bool CanConvert(Type typeToConvert)
+    {
+        return typeToConvert.IsInterface || typeToConvert.IsAbstract;
+    }
+
+    /// <inheritdoc />
+    public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+    {
+        return (JsonConverter)Activator.CreateInstance(
+            typeof(PolymorphicConverter<>).MakeGenericType(typeToConvert),
+            BindingFlags.Instance | BindingFlags.Public,
+            null,
+            new object[] { },
+            null)!;
+    }
+}
