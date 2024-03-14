6e0a2924e3fa3f0b72db64fb1b4c8656d64dd7d1
Etienne Baudoux <ebaudoux@velersoftware.com>
2024-03-14 14:52:56 -0700

    Ported Number Base Converter to DevToys 2.0 (#1069)

---
+++ src/app/dev/DevToys.Tools/Tools/Converters/NumberBase/INumberBaseConverterGuiToolMode.cs
@@ -0,0 +1,12 @@
+﻿using DevToys.Tools.Models.NumberBase;
+
+namespace DevToys.Tools.Tools.Converters.NumberBase;
+
+internal interface INumberBaseConverterGuiToolMode
+{
+    IUIElement View { get; }
+
+    void OnInputChanged();
+
+    void OnDataReceived(object? parsedData);
+}
