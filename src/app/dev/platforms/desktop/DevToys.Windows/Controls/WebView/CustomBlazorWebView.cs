01ea51e64526b4c21e74d3b9b8c99d7ac39234e8
Etienne Baudoux <ebaudoux@velersoftware.com>
2024-04-09 08:01:19 -0700

    Made DevToys.Blazor embedding resources and excluding them from static web assets

---
+++ src/app/dev/platforms/desktop/DevToys.Windows/Controls/WebView/CustomBlazorWebView.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Wpf;
+using Microsoft.Extensions.FileProviders;
+
+namespace DevToys.Windows.Controls.WebView;
+
+internal sealed class CustomBlazorWebView : BlazorWebView
+{
+    public override IFileProvider CreateFileProvider(string contentRootDir)
+    {
+        System.Reflection.Assembly assembly = typeof(DevToys.Blazor.Main).Assembly;
+        var embeddedProvider = new EmbeddedFileProvider(assembly);
+        var physicalProvider = new PhysicalFileProvider(contentRootDir);
+        return new CompositeFileProvider(physicalProvider, embeddedProvider);
+    }
+}
